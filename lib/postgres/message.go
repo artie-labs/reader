f1756664b3f1556e1ffe8eb681605daeed6c411c
Nathan Villaescusa <nathan@artie.so>
2024-01-29 13:40:35 -0800

    WIP

---
+++ lib/postgres/message.go
@@ -0,0 +1,121 @@
+package postgres
+
+import (
+	"database/sql"
+	"fmt"
+	"log/slog"
+	"strings"
+	"time"
+
+	"github.com/artie-labs/reader/config"
+	"github.com/artie-labs/reader/lib"
+	"github.com/artie-labs/reader/lib/mtr"
+	"github.com/artie-labs/reader/lib/postgres/debezium"
+	"github.com/artie-labs/transfer/lib/size"
+)
+
+const defaultErrorRetries = 10
+
+type MessageBuilder struct {
+	table      *Table
+	scanner    scanner
+	statsD     *mtr.Client
+	maxRowSize uint64
+}
+
+func LoadTable(db *sql.DB, tableCfg *config.PostgreSQLTable, statsD *mtr.Client, maxRowSize uint64) (*MessageBuilder, error) {
+	slog.Info("Loading configuration for table", slog.String("table", tableCfg.Name))
+
+	table := NewTable(tableCfg)
+	if err := table.RetrieveColumns(db); err != nil {
+		if NoRowsError(err) {
+			slog.Info("Table does not contain any rows, skipping...", slog.String("table", tableCfg.Name))
+			return nil, nil
+		} else {
+			return nil, fmt.Errorf("failed to validate postgres: %w", err)
+		}
+	}
+
+	slog.Info("Scanning table",
+		slog.String("tableName", table.Name),
+		slog.String("schemaName", table.Schema),
+		slog.String("topicSuffix", table.TopicSuffix()),
+		slog.Any("primaryKeyColumns", table.PrimaryKeys.Keys()),
+		slog.Any("batchSize", tableCfg.GetLimit()),
+	)
+
+	scanner := table.NewScanner(db, tableCfg.GetLimit(), defaultErrorRetries)
+	return NewMessageBuilder(
+		table,
+		&scanner,
+		statsD,
+		maxRowSize,
+	), nil
+}
+
+func NewMessageBuilder(table *Table, _scanner *scanner, statsD *mtr.Client, maxRowSize uint64) *MessageBuilder {
+	return &MessageBuilder{
+		table:      table,
+		scanner:    *_scanner,
+		statsD:     statsD,
+		maxRowSize: maxRowSize,
+	}
+}
+
+func (m *MessageBuilder) HasNext() bool {
+	return m != nil && m.scanner.HasNext()
+}
+
+func (m *MessageBuilder) recordMetrics(start time.Time) {
+	if m.statsD != nil {
+		(*m.statsD).Timing("scanned_and_parsed", time.Since(start), map[string]string{
+			"table":  strings.ReplaceAll(m.table.Name, `"`, ``),
+			"schema": m.table.Schema,
+		})
+	}
+}
+
+func (m *MessageBuilder) Next() ([]lib.RawMessage, error) {
+	if !m.HasNext() {
+		return make([]lib.RawMessage, 0), nil
+	}
+
+	rows, err := m.scanner.Next()
+	if err != nil {
+		return nil, fmt.Errorf("failed to scan postgres: %w", err)
+	}
+
+	var result []lib.RawMessage
+	for _, row := range rows {
+		start := time.Now()
+		partitionKeyMap := make(map[string]interface{})
+		for _, key := range m.table.PrimaryKeys.Keys() {
+			partitionKeyMap[key] = row[key]
+		}
+
+		if m.maxRowSize > 0 {
+			if uint64(size.GetApproxSize(row)) > m.maxRowSize {
+				slog.Info(fmt.Sprintf("Row greater than %v mb, skipping...", m.maxRowSize/1024/1024), slog.Any("key", partitionKeyMap))
+				continue
+			}
+		}
+
+		payload, err := debezium.NewPayload(&debezium.NewArgs{
+			TableName: m.table.Name,
+			Columns:   m.table.OriginalColumns,
+			Fields:    m.table.Config.Fields,
+			RowData:   row,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("failed to generate payload: %w", err)
+		}
+
+		result = append(result, lib.RawMessage{
+			TopicSuffix:  m.table.TopicSuffix(),
+			PartitionKey: partitionKeyMap,
+			Payload:      payload,
+		})
+		m.recordMetrics(start)
+	}
+	return result, nil
+}
