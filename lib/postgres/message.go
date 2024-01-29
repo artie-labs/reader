9759269bb98347de5d8a587ed486b9a550896760
Nathan <148575555+nathan-artie@users.noreply.github.com>
2024-01-29 15:13:48 -0800

    Move LoadTable to snapshot.go (#64)

--- lib/postgres/message.go
+++ lib/postgres/message.go
@@ -1,20 +1,16 @@
 package postgres
 
 import (
-	"database/sql"
 	"fmt"
 	"log/slog"
 	"strings"
 	"time"
 
-	"github.com/artie-labs/reader/config"
 	"github.com/artie-labs/reader/lib"
 	"github.com/artie-labs/reader/lib/mtr"
 	"github.com/artie-labs/reader/lib/postgres/debezium"
 	"github.com/artie-labs/transfer/lib/size"
 )
-
-const defaultErrorRetries = 10
 
 type MessageBuilder struct {
 	statsD     *mtr.Client
@@ -35,36 +31,6 @@ func NewMessageBuilder(table *Table, iter batchRowIterator, statsD *mtr.Client, 
 type batchRowIterator interface {
 	HasNext() bool
 	Next() ([]map[string]interface{}, error)
-}
-
-func LoadTable(db *sql.DB, tableCfg *config.PostgreSQLTable, statsD *mtr.Client, maxRowSize uint64) (*MessageBuilder, error) {
-	slog.Info("Loading configuration for table", slog.String("table", tableCfg.Name))
-
-	table := NewTable(tableCfg)
-	if err := table.RetrieveColumns(db); err != nil {
-		if NoRowsError(err) {
-			slog.Info("Table does not contain any rows, skipping...", slog.String("table", table.Name))
-			return nil, nil
-		} else {
-			return nil, fmt.Errorf("failed to validate postgres: %w", err)
-		}
-	}
-
-	slog.Info("Scanning table",
-		slog.String("tableName", table.Name),
-		slog.String("schemaName", table.Schema),
-		slog.String("topicSuffix", table.TopicSuffix()),
-		slog.Any("primaryKeyColumns", table.PrimaryKeys.Keys()),
-		slog.Any("batchSize", tableCfg.GetLimit()),
-	)
-
-	scanner := table.NewScanner(db, tableCfg.GetLimit(), defaultErrorRetries)
-	return NewMessageBuilder(
-		table,
-		&scanner,
-		statsD,
-		maxRowSize,
-	), nil
 }
 
 func (m *MessageBuilder) HasNext() bool {
--- sources/postgres/snapshot.go
+++ sources/postgres/snapshot.go
@@ -16,6 +16,38 @@ import (
 	"github.com/artie-labs/reader/lib/postgres"
 )
 
+const defaultErrorRetries = 10
+
+func loadTable(db *sql.DB, tableCfg *config.PostgreSQLTable, statsD *mtr.Client, maxRowSize uint64) (*postgres.MessageBuilder, error) {
+	slog.Info("Loading configuration for table", slog.String("table", tableCfg.Name))
+
+	table := postgres.NewTable(tableCfg)
+	if err := table.RetrieveColumns(db); err != nil {
+		if postgres.NoRowsError(err) {
+			slog.Info("Table does not contain any rows, skipping...", slog.String("table", table.Name))
+			return nil, nil
+		} else {
+			return nil, fmt.Errorf("failed to validate postgres: %w", err)
+		}
+	}
+
+	slog.Info("Scanning table",
+		slog.String("tableName", table.Name),
+		slog.String("schemaName", table.Schema),
+		slog.String("topicSuffix", table.TopicSuffix()),
+		slog.Any("primaryKeyColumns", table.PrimaryKeys.Keys()),
+		slog.Any("batchSize", tableCfg.GetLimit()),
+	)
+
+	scanner := table.NewScanner(db, tableCfg.GetLimit(), defaultErrorRetries)
+	return postgres.NewMessageBuilder(
+		table,
+		&scanner,
+		statsD,
+		maxRowSize,
+	), nil
+}
+
 func Run(ctx context.Context, cfg config.Settings, statsD *mtr.Client, kafkaWriter *kafka.Writer) error {
 	batchWriter := kafkalib.NewBatchWriter(ctx, *cfg.Kafka, kafkaWriter)
 
@@ -27,7 +59,7 @@ func Run(ctx context.Context, cfg config.Settings, statsD *mtr.Client, kafkaWrit
 
 	for _, table := range cfg.PostgreSQL.Tables {
 		snapshotStartTime := time.Now()
-		iter, err := postgres.LoadTable(db, table, statsD, cfg.Kafka.MaxRequestSize)
+		iter, err := loadTable(db, table, statsD, cfg.Kafka.MaxRequestSize)
 		if err != nil {
 			return fmt.Errorf("failed to create table iterator, table: %s, err: %w", table.Name, err)
 		}
