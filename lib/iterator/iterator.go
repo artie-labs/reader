dfe8113f8aede9c87ee6e4ab52195a2ef5690977
Nathan Villaescusa <nathan@artie.so>
2024-01-31 16:53:14 -0800

    PR feedback

--- lib/iterator/iterator.go
+++ lib/iterator/iterator.go
@@ -3,14 +3,14 @@ package iterator
 type batchIterator[T any] struct {
 	items []T
 	index int
-	n     int
+	step  int
 }
 
 func NewBatchIterator[T any](items []T, n int) *batchIterator[T] {
 	return &batchIterator[T]{
 		items: items,
 		index: 0,
-		n:     max(n, 1),
+		step:  max(n, 1),
 	}
 }
 
@@ -20,9 +20,9 @@ func (i *batchIterator[T]) HasNext() bool {
 
 func (i *batchIterator[T]) Next() []T {
 	if !i.HasNext() {
-		return make([]T, 0)
+		return nil
 	}
-	end := min(i.index+i.n, len(i.items))
+	end := min(i.index+i.step, len(i.items))
 	result := i.items[i.index:end]
 	i.index = end
 	return result
