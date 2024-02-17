50af904824019cd5c49495a1ad99dacc4c061827
Nathan Villaescusa <nathan@artie.so>
2024-02-17 13:51:58 -0800

    Combine type checks

--- lib/postgres/temp/parse_value.go
+++ lib/postgres/temp/parse_value.go
@@ -8,17 +8,8 @@ func ParseValue(key string, optionalSchema map[string]typing.KindDetails) bool {
 	if len(optionalSchema) > 0 {
 		// If the column exists in the schema, let's early exit.
 		if kindDetail, isOk := optionalSchema[key]; isOk {
-			// If the schema exists, use it as sot.
-			if kindDetail.Kind == typing.ETime.Kind || kindDetail.Kind == typing.EDecimal.Kind {
-				// If the data type is either `ETime` or `EDecimal` and the value exists, we will not early exit
-				// We are not skipping so that we are able to get the exact layout specified at the row level to preserve:
-				// 1. Layout for time / date / timestamps
-				// 2. Precision and scale for numeric values
-				return ParseValue(key, nil)
-			}
-
 			switch kindDetail.Kind {
-			case typing.String.Kind, typing.Struct.Kind, typing.ETime.Kind:
+			case typing.String.Kind, typing.Struct.Kind, typing.ETime.Kind, typing.EDecimal.Kind:
 				return true
 			default:
 				return false
