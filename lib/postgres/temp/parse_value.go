ee55a458f0538255c7a5d6edfc99e5404d2691af
Nathan Villaescusa <nathan@artie.so>
2024-02-17 13:38:31 -0800

    Change val type to string

--- lib/postgres/temp/parse_value.go
+++ lib/postgres/temp/parse_value.go
@@ -1,25 +1,18 @@
 package temp
 
 import (
-	"reflect"
 	"strings"
 
 	"github.com/artie-labs/transfer/lib/typing"
-	"github.com/artie-labs/transfer/lib/typing/decimal"
 	"github.com/artie-labs/transfer/lib/typing/ext"
 )
 
-func ParseValue(settings typing.Settings, key string, optionalSchema map[string]typing.KindDetails, val interface{}) typing.KindDetails {
-	if val == nil && !settings.CreateAllColumnsIfAvailable {
-		// If the value is nil and `createAllColumnsIfAvailable` = false, then return `Invalid
-		return typing.Invalid
-	}
-
+func ParseValue(settings typing.Settings, key string, optionalSchema map[string]typing.KindDetails, val string) typing.KindDetails {
 	if len(optionalSchema) > 0 {
 		// If the column exists in the schema, let's early exit.
 		if kindDetail, isOk := optionalSchema[key]; isOk {
 			// If the schema exists, use it as sot.
-			if val != nil && (kindDetail.Kind == typing.ETime.Kind || kindDetail.Kind == typing.EDecimal.Kind) {
+			if kindDetail.Kind == typing.ETime.Kind || kindDetail.Kind == typing.EDecimal.Kind {
 				// If the data type is either `ETime` or `EDecimal` and the value exists, we will not early exit
 				// We are not skipping so that we are able to get the exact layout specified at the row level to preserve:
 				// 1. Layout for time / date / timestamps
@@ -31,57 +24,24 @@ func ParseValue(settings typing.Settings, key string, optionalSchema map[string]
 		}
 	}
 
-	switch convertedVal := val.(type) {
-	case nil:
-		return typing.Invalid
-	case uint, int, uint8, uint16, uint32, uint64, int8, int16, int32, int64:
-		return typing.Integer
-	case float32, float64:
-		// Integers will be parsed as Floats if they come from JSON
-		// This is a limitation with JSON - https://github.com/golang/go/issues/56719
-		// UNLESS Transfer is provided with a schema object, and we deliberately typecast the value to an integer
-		// before calling ParseValue().
-		return typing.Float
-	case bool:
-		return typing.Boolean
-	case string:
-		// If it contains space or -, then we must check against date time.
-		// This way, we don't penalize every string into going through this loop
-		// In the future, we can have specific layout RFCs run depending on the char
-		if strings.Contains(convertedVal, ":") || strings.Contains(convertedVal, "-") {
-			extendedKind, err := ext.ParseExtendedDateTime(convertedVal, settings.AdditionalDateFormats)
-			if err == nil {
-				return typing.KindDetails{
-					Kind:                typing.ETime.Kind,
-					ExtendedTimeDetails: &extendedKind.NestedKind,
-				}
+	var convertedVal = val
+
+	// If it contains space or -, then we must check against date time.
+	// This way, we don't penalize every string into going through this loop
+	// In the future, we can have specific layout RFCs run depending on the char
+	if strings.Contains(convertedVal, ":") || strings.Contains(convertedVal, "-") {
+		extendedKind, err := ext.ParseExtendedDateTime(convertedVal, settings.AdditionalDateFormats)
+		if err == nil {
+			return typing.KindDetails{
+				Kind:                typing.ETime.Kind,
+				ExtendedTimeDetails: &extendedKind.NestedKind,
 			}
-		}
-
-		if typing.IsJSON(convertedVal) {
-			return typing.Struct
-		}
-
-		return typing.String
-
-	case *decimal.Decimal:
-		return typing.KindDetails{
-			Kind:                   typing.EDecimal.Kind,
-			ExtendedDecimalDetails: convertedVal,
-		}
-	case *ext.ExtendedTime:
-		return typing.KindDetails{
-			Kind:                typing.ETime.Kind,
-			ExtendedTimeDetails: &convertedVal.NestedKind,
-		}
-	default:
-		// Check if the val is one of our custom-types
-		if reflect.TypeOf(val).Kind() == reflect.Slice {
-			return typing.Array
-		} else if reflect.TypeOf(val).Kind() == reflect.Map {
-			return typing.Struct
 		}
 	}
 
-	return typing.Invalid
+	if typing.IsJSON(convertedVal) {
+		return typing.Struct
+	}
+
+	return typing.String
 }
