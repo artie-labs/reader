139d5009432f43e0a259eca78796cb93dc300495
Nathan Villaescusa <nathan@artie.so>
2024-02-18 12:46:14 -0800

    [mysql] Add snapshot stub

--- lib/postgres/table.go
+++ lib/postgres/table.go
@@ -24,7 +24,7 @@ type Table struct {
 	OptionalPrimaryKeyValEnd   string
 }
 
-func NewTable(cfgTable *config.PostgreSQLTable) *Table {
+func NewTable(cfgTable config.PostgreSQLTable) *Table {
 	return &Table{
 		Name:                       cfgTable.Name,
 		Schema:                     cfgTable.Schema,
--- sources/mysql/snapshot.go
+++ sources/mysql/snapshot.go
@@ -1,4 +1,4 @@
-package postgres
+package mysql
 
 import (
 	"context"
@@ -7,37 +7,33 @@ import (
 	"log/slog"
 	"time"
 
-	_ "github.com/jackc/pgx/v5/stdlib"
+	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/artie-labs/reader/config"
 	"github.com/artie-labs/reader/lib/kafkalib"
 	"github.com/artie-labs/reader/lib/mtr"
-	"github.com/artie-labs/reader/lib/postgres"
+	"github.com/artie-labs/reader/lib/mysql"
 	"github.com/artie-labs/reader/lib/rdbms"
-	"github.com/artie-labs/reader/sources/postgres/transformer"
 )
 
-const defaultErrorRetries = 10
-
 type Source struct {
-	cfg config.PostgreSQL
+	cfg config.MySQL
 	db  *sql.DB
 }
 
-func Load(cfg config.PostgreSQL) (*Source, error) {
-	db, err := sql.Open("pgx", cfg.ToDSN())
+func Load(cfg config.MySQL) (*Source, error) {
+	db, err := sql.Open("mysql", cfg.ToDSN())
 	if err != nil {
-		return nil, fmt.Errorf("failed to connect to PostgreSQL: %w", err)
+		return nil, fmt.Errorf("failed to connect to MySQL: %w", err)
 	}
-
 	return &Source{
 		cfg: cfg,
 		db:  db,
 	}, nil
 }
 
-func (s *Source) Close() error {
-	return s.db.Close()
+func (p Source) Close() error {
+	return p.db.Close()
 }
 
 func (s *Source) Run(ctx context.Context, writer kafkalib.BatchWriter, statsD mtr.Client) error {
@@ -49,15 +45,14 @@ func (s *Source) Run(ctx context.Context, writer kafkalib.BatchWriter, statsD mt
 	return nil
 }
 
-func (s Source) snapshotTable(ctx context.Context, writer kafkalib.BatchWriter, statsD mtr.Client, tableCfg config.PostgreSQLTable) error {
+func (s Source) snapshotTable(ctx context.Context, writer kafkalib.BatchWriter, statsD mtr.Client, tableCfg config.MySQLTable) error {
 	snapshotStartTime := time.Now()
 
 	slog.Info("Loading configuration for table", slog.String("table", tableCfg.Name))
-	table := postgres.NewTable(tableCfg)
+	table := mysql.NewTable(tableCfg)
 	if err := table.PopulateColumns(s.db); err != nil {
 		if rdbms.IsNoRowsErr(err) {
-			slog.Info("Table does not contain any rows, skipping...", slog.String("table", table.Name),
-				slog.String("schema", table.Schema))
+			slog.Info("Table does not contain any rows, skipping...", slog.String("table", table.Name))
 			return nil
 		} else {
 			return fmt.Errorf("failed to load configuration for table %s: %w", table.Name, err)
@@ -66,19 +61,15 @@ func (s Source) snapshotTable(ctx context.Context, writer kafkalib.BatchWriter, 
 
 	slog.Info("Scanning table",
 		slog.String("table", table.Name),
-		slog.String("schema", table.Schema),
 		slog.Any("primaryKeyColumns", table.PrimaryKeys.Keys()),
-		slog.Any("batchSize", tableCfg.GetBatchSize()),
+		slog.Any("batchSize", tableCfg.BatchSize),
 	)
 
-	scanner := table.NewScanner(s.db, tableCfg.GetBatchSize(), defaultErrorRetries)
-	dbzTransformer := transformer.NewDebeziumTransformer(table, &scanner, statsD)
-	count, err := writer.WriteIterator(ctx, dbzTransformer)
-	if err != nil {
-		return fmt.Errorf("failed to snapshot for table %s: %w", table.Name, err)
-	}
+	// TODO: Implement snapshotting logic here
+	count := 0
 
 	slog.Info("Finished snapshotting",
+		slog.String("table", tableCfg.Name),
 		slog.Int("scannedTotal", count),
 		slog.Duration("totalDuration", time.Since(snapshotStartTime)),
 	)
