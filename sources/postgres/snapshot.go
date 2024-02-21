63cb83d0b58d133669a3c17935bbfccc19ad27b4
Nathan Villaescusa <nathan@artie.so>
2024-02-21 12:15:31 -0800

    [mysql,postgres] Create `RetryConfig` when building scanners

--- sources/postgres/snapshot.go
+++ sources/postgres/snapshot.go
@@ -66,7 +66,10 @@ func (s *Source) Run(ctx context.Context, writer kafkalib.BatchWriter) error {
 			slog.Any("batchSize", tableCfg.GetBatchSize()),
 		)
 
-		scanner := table.NewScanner(s.db, tableCfg.GetBatchSize(), defaultErrorRetries)
+		scanner, err := table.NewScanner(s.db, tableCfg.GetBatchSize(), defaultErrorRetries)
+		if err != nil {
+			return fmt.Errorf("failed to build scanner for table %s: %w", table.Name, err)
+		}
 		dbzTransformer := debezium.NewDebeziumTransformer(adapter.NewPostgresAdapter(*table), &scanner)
 		count, err := writer.WriteIterator(ctx, dbzTransformer)
 		if err != nil {
