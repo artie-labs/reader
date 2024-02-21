8d132e1e1e0ae4ede23e6f7acdf3467b920c2a28
Nathan <148575555+nathan-artie@users.noreply.github.com>
2024-02-21 14:58:41 -0800

    [mysql] Fix primary keys bug; build scanner query (#181)

--- lib/mysql/scanner/scanner.go
+++ lib/mysql/scanner/scanner.go
@@ -3,6 +3,7 @@ package scanner
 import (
 	"database/sql"
 	"fmt"
+	"log/slog"
 
 	"github.com/artie-labs/reader/lib/mysql"
 	"github.com/artie-labs/reader/lib/rdbms/primary_key"
@@ -22,7 +23,10 @@ type scanner struct {
 	retryCfg  retry.RetryConfig
 
 	// mutable
-	primaryKeys *primary_key.Keys
+	primaryKeys  *primary_key.Keys
+	isFirstBatch bool
+	isLastBatch  bool
+	done         bool
 }
 
 func NewScanner(db *sql.DB, table mysql.Table, batchSize uint, errorRetries int) (scanner, error) {
@@ -32,18 +36,61 @@ func NewScanner(db *sql.DB, table mysql.Table, batchSize uint, errorRetries int)
 	}
 
 	return scanner{
-		db:          db,
-		table:       table,
-		batchSize:   batchSize,
-		retryCfg:    retryCfg,
-		primaryKeys: table.PrimaryKeys.Clone(),
+		db:           db,
+		table:        table,
+		batchSize:    batchSize,
+		retryCfg:     retryCfg,
+		primaryKeys:  table.PrimaryKeys.Clone(),
+		isFirstBatch: true,
+		isLastBatch:  false,
+		done:         false,
 	}, nil
 }
 
 func (s *scanner) HasNext() bool {
-	panic("not implemented")
+	return !s.done
 }
 
 func (s *scanner) Next() ([]map[string]interface{}, error) {
+	if !s.HasNext() {
+		return nil, fmt.Errorf("no more rows to scan")
+	}
+
+	rows, err := s.scan()
+	if err != nil {
+		s.done = true
+		return nil, err
+	}
+
+	if len(rows) == 0 {
+		slog.Info("Finished scanning", slog.String("table", s.table.Name))
+		s.done = true
+		return nil, nil
+	}
+
+	s.isFirstBatch = false
+	// The reason why `isLastBatch`` exists is because in the past, we had queries only return partial results but it wasn't fully done
+	s.isLastBatch = s.batchSize > uint(len(rows))
+
+	return rows, nil
+}
+
+func (s *scanner) scan() ([]map[string]interface{}, error) {
+	query, parameters, err := buildScanTableQuery(buildScanTableQueryArgs{
+		TableName:   s.table.Name,
+		PrimaryKeys: s.primaryKeys,
+		Columns:     s.table.Columns,
+
+		InclusiveLowerBound: s.isFirstBatch,
+		InclusiveUpperBound: !s.isLastBatch,
+
+		Limit: s.batchSize,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate query: %w", err)
+	}
+
+	slog.Info("Scan query", slog.String("query", query), slog.Any("parameters", parameters))
+
 	panic("not implemented")
 }
