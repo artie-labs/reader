2e689f4b198c0b3cb524e00c8172f7936c8454e6
Nathan Villaescusa <nathan@artie.so>
2024-03-02 02:20:03 -0800

    Simpler

--- lib/postgres/scanner.go
+++ lib/postgres/scanner.go
@@ -212,13 +212,10 @@ func (s scanAdapter) ParseRow(values []any) (map[string]any, error) {
 	row := make(map[string]any)
 	for idx, v := range values {
 		col := s.columns[idx]
-
-		value, err := parse.ParseValue(col.Type, v)
-		if err != nil {
+		var err error
+		if row[col.Name], err = parse.ParseValue(col.Type, v); err != nil {
 			return nil, err
 		}
-
-		row[col.Name] = value
 	}
 	return row, nil
 }
