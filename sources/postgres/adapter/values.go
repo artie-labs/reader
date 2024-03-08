11f132ae79bfa99d0ac68c8ec8ba010d34f4aa4d
Nathan Villaescusa <nathan@artie.so>
2024-03-08 11:43:09 -0800

    [postgres] Add `PgTimestampConverter`

--- sources/postgres/adapter/values.go
+++ sources/postgres/adapter/values.go
@@ -1,9 +1,6 @@
 package adapter
 
 import (
-	"log/slog"
-	"time"
-
 	"github.com/artie-labs/reader/lib/postgres/schema"
 )
 
@@ -14,19 +11,6 @@ func ConvertValueToDebezium(col schema.Column, value any) (any, error) {
 
 	if converter := valueConverterForType(col.Type, col.Opts); converter != nil {
 		return converter.Convert(value)
-	}
-
-	switch col.Type {
-	case schema.Timestamp:
-		valTime, isOk := value.(time.Time)
-		if isOk {
-			if valTime.Year() > 9999 || valTime.Year() < 0 {
-				// Avoid copying this column over because it'll cause a JSON Marshal error:
-				// Time.MarshalJSON: year outside of range [0,9999]
-				slog.Info("Skipping timestamp because year is greater than 9999 or less than 0", slog.String("key", col.Name), slog.Any("value", value))
-				return nil, nil
-			}
-		}
 	}
 
 	return value, nil
