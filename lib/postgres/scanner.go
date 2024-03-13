b4e973b6f5ee37a6758a8632c199652e634b8b1b
Nathan Villaescusa <nathan@artie.so>
2024-03-13 12:50:14 -0700

    Rename

--- lib/postgres/scanner.go
+++ lib/postgres/scanner.go
@@ -256,9 +256,9 @@ func (s scanAdapter) BuildQuery(primaryKeys []primary_key.Key, isFirstBatch bool
 }
 
 func (s scanAdapter) ParseRow(values []any) error {
-	for i, v := range values {
+	for i, value := range values {
 		var err error
-		values[i], err = parse.ParseValue(s.columns[i].Type, v)
+		values[i], err = parse.ParseValue(s.columns[i].Type, value)
 		if err != nil {
 			return err
 		}
