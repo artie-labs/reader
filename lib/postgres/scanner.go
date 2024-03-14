65de163bc62d09376a0bdf5160fbd40688857bd1
Nathan Villaescusa <nathan@artie.so>
2024-03-14 16:25:07 -0700

    Remove comment

--- lib/postgres/scanner.go
+++ lib/postgres/scanner.go
@@ -36,7 +36,6 @@ var supportedPrimaryKeyDataType []schema.DataType = []schema.DataType{
 	schema.UUID,
 	schema.Inet,
 	schema.JSON,
-	// schema.Bit - fails: operator does not exist: bit >= boolean (SQLSTATE 42883)
 	// schema.Bytea - fails: invalid byte sequence for encoding
 	// schema.TimeWithTimeZone - fails: without the original timezone offset the query doesn't match any rows
 	// schema.Array - fails: this doesn't work: need to serialize to Postgres array format "{1,2,3}"
