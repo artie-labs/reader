287ae5b20453cac7f49d14b16f72a46ff06657ab
Nathan <148575555+nathan-artie@users.noreply.github.com>
2024-03-14 10:16:27 -0700

    [postgres] Remove unused code (#291)

--- lib/postgres/query_test.go
+++ lib/postgres/query_test.go
@@ -6,10 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestQuotedIdentifiers(t *testing.T) {
-	assert.Equal(t, []string{`"a"`, `"bb""bb"`, `"c"`}, QuotedIdentifiers([]string{"a", `bb"bb`, "c"}))
-}
-
 func TestQuoteLiteral(t *testing.T) {
 	assert.Equal(t, "'abc'", QuoteLiteral("abc"))
 	assert.Equal(t, "'a''bc'", QuoteLiteral("a'bc"))
