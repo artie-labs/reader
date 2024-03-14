a073a07aa2b67b569dcaa0848534b20b055b0b71
Nathan Villaescusa <nathan@artie.so>
2024-03-13 21:15:47 -0700

    remove a s

--- lib/postgres/query_test.go
+++ lib/postgres/query_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestQueryPlaceholderss(t *testing.T) {
+func TestQueryPlaceholders(t *testing.T) {
 	assert.Equal(t, []string{}, QueryPlaceholders(0, 0))
 	assert.Equal(t, []string{"$1", "$2"}, QueryPlaceholders(0, 2))
 	assert.Equal(t, []string{"$4", "$5", "$6", "$7"}, QueryPlaceholders(3, 4))
