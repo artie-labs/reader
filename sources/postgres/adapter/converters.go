da57cad8ae962739dccf3d439883830cf65548b6
Nathan <148575555+nathan-artie@users.noreply.github.com>
2024-03-17 11:40:11 -0700

    [postgres] Kill `PgTimestampConverter` (#295)

--- sources/postgres/adapter/converters.go
+++ sources/postgres/adapter/converters.go
@@ -2,7 +2,6 @@ package adapter
 
 import (
 	"fmt"
-	"log/slog"
 	"math"
 	"time"
 
@@ -62,34 +61,6 @@ func (PgTimeConverter) Convert(value any) (any, error) {
 		return nil, fmt.Errorf("milliseconds overflows int32")
 	}
 	return int32(milliseconds), nil
-}
-
-// TODO: Replace this with `converters.TimestampConverter` once we've run it for a while and not seen error logs
-type PgTimestampConverter struct{}
-
-func (PgTimestampConverter) ToField(name string) transferDbz.Field {
-	return transferDbz.Field{
-		FieldName:    name,
-		DebeziumType: string(transferDbz.Timestamp),
-		// NOTE: We are returning string here because we want the right layout to be used by our Typing library
-		Type: "string",
-	}
-}
-
-func (PgTimestampConverter) Convert(value any) (any, error) {
-	valTime, isOk := value.(time.Time)
-	if isOk {
-		if valTime.Year() > 9999 || valTime.Year() < 0 {
-			// Avoid copying this column over because it'll cause a JSON Marshal error:
-			// Time.MarshalJSON: year outside of range [0,9999]
-			slog.Info("Skipping timestamp because year is greater than 9999 or less than 0", slog.Any("value", value))
-			return nil, nil
-		}
-	} else {
-		slog.Error("Emitting a value for a timestamp column that is not a time.Time", slog.Any("value", value), slog.String("type", fmt.Sprintf("%T", value)))
-	}
-
-	return value, nil
 }
 
 type PgIntervalConverter struct{}
