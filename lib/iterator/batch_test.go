741c43da8d45375ec6a629c20b29de8447ca07a8
Nathan Villaescusa <nathan@artie.so>
2024-04-01 22:07:11 -0700

    Test

--- lib/iterator/batch_test.go
+++ lib/iterator/batch_test.go
@@ -7,34 +7,39 @@ import (
 )
 
 func TestBatchIterator(t *testing.T) {
-	// length of items is 0
+	// Length of items is 0
 	{
 		batches, err := Collect(Batched(ForSlice([]int{}), 2))
 		assert.NoError(t, err)
 		assert.Empty(t, batches)
 	}
-	// length of items is 1
+	// Length of items is 1
 	{
 		batches, err := Collect(Batched(ForSlice([]int{1}), 2))
 		assert.NoError(t, err)
 		assert.Equal(t, [][]int{{1}}, batches)
 	}
-	// n is 0
+	// N is 0
 	{
 		batches, err := Collect(Batched(ForSlice([]int{1, 2}), 0))
 		assert.NoError(t, err)
 		assert.Equal(t, [][]int{{1}, {2}}, batches)
 	}
-	// length of items is a multiple of n
+	// Length of items is a multiple of n
 	{
 		batches, err := Collect(Batched(ForSlice([]int{1, 2, 3, 4}), 2))
 		assert.NoError(t, err)
 		assert.Equal(t, [][]int{{1, 2}, {3, 4}}, batches)
 	}
-	// length of items is not a multiple of n
+	// Length of items is not a multiple of n
 	{
 		batches, err := Collect(Batched(ForSlice([]int{1, 2, 3, 4, 5}), 2))
 		assert.NoError(t, err)
 		assert.Equal(t, [][]int{{1, 2}, {3, 4}, {5}}, batches)
+	}
+	// Error that happens immediately.
+	{
+		_, err := Collect(Batched(errorIterator{}, 2))
+		assert.ErrorContains(t, err, "error in Next()")
 	}
 }
