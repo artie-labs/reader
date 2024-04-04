2f11512899817120cc13509fa6cf3965470c3489
Nathan Villaescusa <nathan@artie.so>
2024-04-04 12:53:38 -0700

    Revert

--- lib/kafkalib/batch.go
+++ lib/kafkalib/batch.go
@@ -2,7 +2,7 @@ package kafkalib
 
 // Batched splits a slice of items into a slice of step-sized slices.
 func Batched[T any](items []T, step int) [][]T {
-	step = max(1, step)
+	step = max(step, 1)
 	var result [][]T
 	for index := 0; index < len(items); {
 		end := min(index+step, len(items))
