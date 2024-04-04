8fe2fddb2ff3a5ee0bc2bfc2463c40ad6dae783d
Nathan Villaescusa <nathan@artie.so>
2024-04-04 15:44:50 -0700

    More tests

--- lib/kafkalib/errors_test.go
+++ lib/kafkalib/errors_test.go
@@ -3,8 +3,9 @@ package kafkalib
 import (
 	"context"
 	"fmt"
-	"github.com/segmentio/kafka-go"
 	"testing"
+
+	"github.com/segmentio/kafka-go"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -24,6 +25,14 @@ func TestIsExceedMaxMessageBytesErr(t *testing.T) {
 		},
 		{
 			err:      fmt.Errorf("Message Size Too Large: the server has a configurable maximum message size to avoid unbounded memory allocation and the client attempted to produce a message larger than this maximum, bytes: 1223213213"),
+			expected: true,
+		},
+		{
+			err:      kafka.TopicAuthorizationFailed,
+			expected: false,
+		},
+		{
+			err:      kafka.MessageSizeTooLarge,
 			expected: true,
 		},
 	}
