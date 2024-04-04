e62a8cfa7a6d8549e948dce417e329515e5c0b07
Nathan Villaescusa <nathan@artie.so>
2024-04-04 15:25:39 -0700

    Change message

--- lib/writer/writer.go
+++ lib/writer/writer.go
@@ -43,9 +43,9 @@ func (w *Writer) Write(ctx context.Context, iter iterator.Iterator[[]lib.RawMess
 			)
 		}
 	}
-	
+
 	if err := w.destination.OnFinish(); err != nil {
-		return 0, fmt.Errorf("failed running onFinish: %w", err)
+		return 0, fmt.Errorf("failed running destination OnFinish: %w", err)
 	}
 
 	return count, nil
