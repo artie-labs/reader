76570bb5952ffae2add8039f3110fa9144796201
Robin Tang <rtang.cs@gmail.com>
2024-05-16 13:46:05 -0700

    Adding time converter.

--- lib/debezium/converters/time.go
+++ lib/debezium/converters/time.go
@@ -8,6 +8,37 @@ import (
 	"github.com/artie-labs/transfer/lib/debezium"
 )
 
+type TimeConverter struct{}
+
+func (TimeConverter) ToField(name string) debezium.Field {
+	return debezium.Field{
+		FieldName:    name,
+		Type:         debezium.Int32,
+		DebeziumType: debezium.Time,
+	}
+}
+
+func (TimeConverter) Convert(value any) (any, error) {
+	var timeValue time.Time
+	switch castedValue := value.(type) {
+	case time.Time:
+		timeValue = castedValue
+	case string:
+		var err error
+		timeValue, err = time.Parse(time.TimeOnly, castedValue)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		return nil, fmt.Errorf("expected string/time.Time got %T with value: %v", value, value)
+	}
+
+	hours := time.Duration(timeValue.Hour()) * time.Hour
+	minutes := time.Duration(timeValue.Minute()) * time.Minute
+	seconds := time.Duration(timeValue.Second()) * time.Second
+	return int32((hours + minutes + seconds) / time.Millisecond), nil
+}
+
 type MicroTimeConverter struct{}
 
 func (MicroTimeConverter) ToField(name string) debezium.Field {
@@ -20,19 +51,46 @@ func (MicroTimeConverter) ToField(name string) debezium.Field {
 }
 
 func (MicroTimeConverter) Convert(value any) (any, error) {
-	strValue, ok := value.(string)
-	if !ok {
-		return nil, fmt.Errorf("expected string got %T with value: %v", value, value)
-	}
-	timeValue, err := time.Parse(time.TimeOnly, strValue)
-	if err != nil {
-		return nil, err
+	var timeValue time.Time
+	switch castedValue := value.(type) {
+	case time.Time:
+		timeValue = castedValue
+	case string:
+		var err error
+		timeValue, err = time.Parse(time.TimeOnly, castedValue)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		return nil, fmt.Errorf("expected string/time.Time got %T with value: %v", value, value)
 	}
 
 	hours := time.Duration(timeValue.Hour()) * time.Hour
 	minutes := time.Duration(timeValue.Minute()) * time.Minute
 	seconds := time.Duration(timeValue.Second()) * time.Second
 	return int64((hours + minutes + seconds) / time.Microsecond), nil
+}
+
+type NanoTimeConverter struct{}
+
+func (NanoTimeConverter) ToField(name string) debezium.Field {
+	return debezium.Field{
+		FieldName:    name,
+		Type:         debezium.Int64,
+		DebeziumType: debezium.NanoTime,
+	}
+}
+
+func (NanoTimeConverter) Convert(value any) (any, error) {
+	timeValue, ok := value.(time.Time)
+	if !ok {
+		return nil, fmt.Errorf("expected time.Time got %T with value: %v", value, value)
+	}
+
+	hours := time.Duration(timeValue.Hour()) * time.Hour
+	minutes := time.Duration(timeValue.Minute()) * time.Minute
+	seconds := time.Duration(timeValue.Second()) * time.Second
+	return int64((hours+minutes+seconds)/time.Microsecond) * 1_000, nil
 }
 
 type DateConverter struct{}
@@ -84,6 +142,51 @@ func (MicroTimestampConverter) Convert(value any) (any, error) {
 		return nil, fmt.Errorf("expected time.Time got %T with value: %v", value, value)
 	}
 	return timeValue.UnixMicro(), nil
+}
+
+type NanoTimestampConverter struct{}
+
+func (NanoTimestampConverter) ToField(name string) debezium.Field {
+	return debezium.Field{
+		FieldName:    name,
+		Type:         debezium.Int64,
+		DebeziumType: debezium.NanoTimestamp,
+	}
+}
+
+func (NanoTimestampConverter) Convert(value any) (any, error) {
+	timeValue, ok := value.(time.Time)
+	if !ok {
+		return nil, fmt.Errorf("expected time.Time got %T with value: %v", value, value)
+	}
+	return timeValue.UnixMicro() * 1_000, nil
+}
+
+type TimestampConverter struct{}
+
+func (TimestampConverter) ToField(name string) debezium.Field {
+	// Represents the number of milliseconds since the epoch, and does not include timezone information.
+	return debezium.Field{
+		FieldName:    name,
+		Type:         debezium.Int64,
+		DebeziumType: debezium.Timestamp,
+	}
+}
+
+func (TimestampConverter) Convert(value any) (any, error) {
+	timeValue, ok := value.(time.Time)
+	if !ok {
+		return nil, fmt.Errorf("expected time.Time got %T with value: %v", value, value)
+	}
+
+	if timeValue.Year() > 9999 || timeValue.Year() < 0 {
+		// Avoid copying this column over because it'll cause a JSON Marshal error:
+		// Time.MarshalJSON: year outside of range [0,9999]
+		slog.Info("Skipping timestamp because year is greater than 9999 or less than 0", slog.Any("value", value))
+		return nil, nil
+	}
+
+	return timeValue.UnixMilli(), nil
 }
 
 type ZonedTimestampConverter struct{}
