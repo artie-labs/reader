2da8a9a2aceae51284b9e34c8c71f00ea126efca
Robin Tang <rtang.cs@gmail.com>
2024-05-16 21:58:27 -0700

    Remove Bounds

--- lib/postgres/schema/schema.go
+++ lib/postgres/schema/schema.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"github.com/artie-labs/reader/lib/rdbms/primary_key"
 	"log/slog"
 	"strings"
 
@@ -246,12 +247,7 @@ func getPrimaryKeyValues(db *sql.DB, schema, table string, primaryKeys []Column,
 	return result, nil
 }
 
-type Bounds struct {
-	Min any
-	Max any
-}
-
-func GetPrimaryKeysBounds(db *sql.DB, schema, table string, primaryKeys []Column) ([]Bounds, error) {
+func GetPrimaryKeysBounds(db *sql.DB, schema, table string, primaryKeys []Column) ([]primary_key.Bounds, error) {
 	minValues, err := getPrimaryKeyValues(db, schema, table, primaryKeys, false)
 	if err != nil {
 		return nil, fmt.Errorf("failed to retrieve lower bounds for primary keys: %w", err)
@@ -262,9 +258,9 @@ func GetPrimaryKeysBounds(db *sql.DB, schema, table string, primaryKeys []Column
 		return nil, fmt.Errorf("failed to retrieve upper bounds for primary keys: %w", err)
 	}
 
-	var bounds []Bounds
+	var bounds []primary_key.Bounds
 	for idx, minValue := range minValues {
-		bounds = append(bounds, Bounds{
+		bounds = append(bounds, primary_key.Bounds{
 			Min: minValue,
 			Max: maxValues[idx],
 		})
