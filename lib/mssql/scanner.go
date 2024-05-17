7d30b098c55347621c4b37ec5cdeec5206bbbe0d
Robin Tang <rtang.cs@gmail.com>
2024-05-17 13:55:09 -0700

    Clean.

--- lib/mssql/scanner.go
+++ lib/mssql/scanner.go
@@ -64,6 +64,7 @@ type scanAdapter struct {
 }
 
 func (s scanAdapter) ParsePrimaryKeyValue(columnName string, value string) (any, error) {
+	// TODO: Implement Date, Time, Datetime for primary key types.
 	columnIdx := slices.IndexFunc(s.columns, func(x schema.Column) bool { return x.Name == columnName })
 	if columnIdx < 0 {
 		return nil, fmt.Errorf("primary key column does not exist: %q", columnName)
