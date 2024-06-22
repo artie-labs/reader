e49a9508114806c1afd69652fdabb7fa62ee58a7
Robin Tang <rtang.cs@gmail.com>
2024-06-22 15:13:25 -0700

    Support Geometry

--- lib/mysql/schema/convert.go
+++ lib/mysql/schema/convert.go
@@ -1,162 +1,179 @@
 package schema
 
 import (
-	"encoding/binary"
-	"fmt"
-	"math"
-	"time"
+    "encoding/binary"
+    "fmt"
+    "math"
+    "time"
 )
 
 const DateTimeFormat = "2006-01-02 15:04:05.999999999"
 
 // ConvertValue takes a value returned from the MySQL driver and converts it to a native Go type.
 func ConvertValue(value any, colType DataType) (any, error) {
-	if value == nil {
-		return nil, nil
-	}
+    if value == nil {
+        return nil, nil
+    }
 
-	switch colType {
-	case Bit:
-		// Bits
-		castValue, ok := value.([]byte)
-		if !ok {
-			return nil, fmt.Errorf("expected []byte got %T for value: %v", value, value)
-		}
-		if len(castValue) != 1 || castValue[0] > 1 {
-			return nil, fmt.Errorf("bit value is invalid: %v", value)
-		}
-		return castValue[0] == 1, nil
-	case Boolean:
-		castVal, ok := value.(int64)
-		if !ok {
-			return nil, fmt.Errorf("expected int64 got %T for value: %v", value, value)
-		}
-		if castVal > 1 || castVal < 0 {
-			return nil, fmt.Errorf("boolean value %d not in [0, 1]", castVal)
-		}
-		return castVal == 1, nil
-	case TinyInt,
-		SmallInt,
-		MediumInt,
-		Int,
-		BigInt,
-		Year:
-		// Types we expect as int64
-		_, ok := value.(int64)
-		if !ok {
-			return nil, fmt.Errorf("expected int64 got %T for value: %v", value, value)
-		}
-		return value, nil
-	case Float:
-		// Types we expect as float32
-		_, ok := value.(float32)
-		if !ok {
-			return nil, fmt.Errorf("expected float32 got %T for value: %v", value, value)
-		}
-		return value, nil
-	case Double:
-		// Types we expect as float64
-		_, ok := value.(float64)
-		if !ok {
-			return nil, fmt.Errorf("expected float64 got %T for value: %v", value, value)
-		}
-		return value, nil
-	case Binary, Varbinary, Blob:
-		// Types we expect as a byte array
-		_, ok := value.([]byte)
-		if !ok {
-			return nil, fmt.Errorf("expected []byte got %T for value: %v", value, value)
-		}
-		return value, nil
-	case Date:
-		bytesValue, ok := value.([]byte)
-		if !ok {
-			return nil, fmt.Errorf("expected []byte got %T for value: %v", value, value)
-		}
+    switch colType {
+    case Bit:
+        // Bits
+        castValue, ok := value.([]byte)
+        if !ok {
+            return nil, fmt.Errorf("expected []byte got %T for value: %v", value, value)
+        }
+        if len(castValue) != 1 || castValue[0] > 1 {
+            return nil, fmt.Errorf("bit value is invalid: %v", value)
+        }
+        return castValue[0] == 1, nil
+    case Boolean:
+        castVal, ok := value.(int64)
+        if !ok {
+            return nil, fmt.Errorf("expected int64 got %T for value: %v", value, value)
+        }
+        if castVal > 1 || castVal < 0 {
+            return nil, fmt.Errorf("boolean value %d not in [0, 1]", castVal)
+        }
+        return castVal == 1, nil
+    case TinyInt,
+        SmallInt,
+        MediumInt,
+        Int,
+        BigInt,
+        Year:
+        // Types we expect as int64
+        _, ok := value.(int64)
+        if !ok {
+            return nil, fmt.Errorf("expected int64 got %T for value: %v", value, value)
+        }
+        return value, nil
+    case Float:
+        // Types we expect as float32
+        _, ok := value.(float32)
+        if !ok {
+            return nil, fmt.Errorf("expected float32 got %T for value: %v", value, value)
+        }
+        return value, nil
+    case Double:
+        // Types we expect as float64
+        _, ok := value.(float64)
+        if !ok {
+            return nil, fmt.Errorf("expected float64 got %T for value: %v", value, value)
+        }
+        return value, nil
+    case Binary, Varbinary, Blob:
+        // Types we expect as a byte array
+        _, ok := value.([]byte)
+        if !ok {
+            return nil, fmt.Errorf("expected []byte got %T for value: %v", value, value)
+        }
+        return value, nil
+    case Date:
+        bytesValue, ok := value.([]byte)
+        if !ok {
+            return nil, fmt.Errorf("expected []byte got %T for value: %v", value, value)
+        }
 
-		// MySQL supports 0000-00-00 for dates so we can't use time.Time
-		return string(bytesValue), nil
-	case DateTime, Timestamp:
-		bytesValue, ok := value.([]byte)
-		if !ok {
-			return nil, fmt.Errorf("expected []byte got %T for value: %v", value, value)
-		}
+        // MySQL supports 0000-00-00 for dates so we can't use time.Time
+        return string(bytesValue), nil
+    case DateTime, Timestamp:
+        bytesValue, ok := value.([]byte)
+        if !ok {
+            return nil, fmt.Errorf("expected []byte got %T for value: %v", value, value)
+        }
 
-		if string(bytesValue) == "0000-00-00 00:00:00" {
-			// MySQL supports '0000-00-00 00:00:00' for datetime columns.
-			// We are returning `nil` here because this will fail most Time parsers.
-			return nil, nil
-		}
+        if string(bytesValue) == "0000-00-00 00:00:00" {
+            // MySQL supports '0000-00-00 00:00:00' for datetime columns.
+            // We are returning `nil` here because this will fail most Time parsers.
+            return nil, nil
+        }
 
-		timeValue, err := time.Parse(DateTimeFormat, string(bytesValue))
-		if err != nil {
-			return nil, err
-		}
-		return timeValue, nil
-	case Decimal,
-		Time,
-		Char,
-		Varchar,
-		Text,
-		TinyText,
-		MediumText,
-		LongText,
-		Enum,
-		Set,
-		JSON:
-		// Types that we expect as a byte array that will be converted to strings
-		switch castValue := value.(type) {
-		case []byte:
-			return string(castValue), nil
-		case string:
-			// The driver should return these these types as []byte but no reason not to support strings too
-			return castValue, nil
-		default:
-			return nil, fmt.Errorf("expected []byte got %T for value: %v", value, value)
-		}
-	case Point:
-		bytes, ok := value.([]byte)
-		if !ok {
-			return nil, fmt.Errorf("expected []byte got %T for value: %v", value, value)
-		}
+        timeValue, err := time.Parse(DateTimeFormat, string(bytesValue))
+        if err != nil {
+            return nil, err
+        }
+        return timeValue, nil
+    case Decimal,
+        Time,
+        Char,
+        Varchar,
+        Text,
+        TinyText,
+        MediumText,
+        LongText,
+        Enum,
+        Set,
+        JSON:
+        // Types that we expect as a byte array that will be converted to strings
+        switch castValue := value.(type) {
+        case []byte:
+            return string(castValue), nil
+        case string:
+            // The driver should return these these types as []byte but no reason not to support strings too
+            return castValue, nil
+        default:
+            return nil, fmt.Errorf("expected []byte got %T for value: %v", value, value)
+        }
+    case Point:
+        bytes, ok := value.([]byte)
+        if !ok {
+            return nil, fmt.Errorf("expected []byte got %T for value: %v", value, value)
+        }
 
-		// Byte format is https://dev.mysql.com/doc/refman/8.4/en/gis-data-formats.html#:~:text=the%20OpenGIS%20specification.-,Internal%20Geometry%20Storage%20Format,-MySQL%20stores%20geometry
-		if len(bytes) != 25 {
-			return nil, fmt.Errorf("expected []byte with length 25, length is %d", len(bytes))
-		}
+        // Byte format is https://dev.mysql.com/doc/refman/8.4/en/gis-data-formats.html#:~:text=the%20OpenGIS%20specification.-,Internal%20Geometry%20Storage%20Format,-MySQL%20stores%20geometry
+        if len(bytes) != 25 {
+            return nil, fmt.Errorf("expected []byte with length 25, length is %d", len(bytes))
+        }
 
-		// The first four bytes are the SRID.
+        // The first four bytes are the SRID.
+        if byteOrder := bytes[5]; byteOrder != 1 {
+            return nil, fmt.Errorf("expected byte order to be 1 (little-endian), byte order is %d", byteOrder)
+        }
 
-		if byteOrder := bytes[5]; byteOrder != 1 {
-			return nil, fmt.Errorf("expected byte order to be 1 (little-endian), byte order is %d", byteOrder)
-		}
+        if integerType := binary.LittleEndian.Uint32(bytes[5:9]); integerType != 1 {
+            return nil, fmt.Errorf("expected integer type 1 (POINT), got %d", integerType)
+        }
 
-		if integerType := binary.LittleEndian.Uint32(bytes[5:9]); integerType != 1 {
-			return nil, fmt.Errorf("expected integer type 1 (POINT), got %d", integerType)
-		}
+        return map[string]any{
+            "x": math.Float64frombits(binary.LittleEndian.Uint64(bytes[9:17])),
+            "y": math.Float64frombits(binary.LittleEndian.Uint64(bytes[17:25])),
+        }, nil
+    case Geometry:
+        // MySQL stores geometry values using 4 bytes to indicate the SRID followed by the WKB representation of the value
+        bytes, ok := value.([]byte)
+        if !ok {
+            return nil, fmt.Errorf("expected []byte got %T for value: %v", value, value)
+        }
+        if len(bytes) < 9 {
+            return nil, fmt.Errorf("expected []byte with minimum length 9, length is %d", len(bytes))
+        }
+        byteOrder := bytes[4]
+        if byteOrder != 1 && byteOrder != 0 {
+            return nil, fmt.Errorf("expected byte order to be 1 (little-endian) or 0 (big-endian), got %d", byteOrder)
+        }
 
-		return map[string]any{
-			"x": math.Float64frombits(binary.LittleEndian.Uint64(bytes[9:17])),
-			"y": math.Float64frombits(binary.LittleEndian.Uint64(bytes[17:25])),
-		}, nil
-	}
+        return map[string]any{
+            "wkb":  bytes[4:],
+            "srid": binary.LittleEndian.Uint32(bytes[0:4]),
+        }, nil
+    }
 
-	return nil, fmt.Errorf("could not convert DataType(%d) %T value: %v", colType, value, value)
+    return nil, fmt.Errorf("could not convert DataType(%d) %T value: %v", colType, value, value)
 }
 
 // ConvertValues takes values returned from the MySQL driver and converts them to native Go types.
 func ConvertValues(values []any, cols []Column) error {
-	if len(values) != len(cols) {
-		return fmt.Errorf("values and cols are not the same length")
-	}
+    if len(values) != len(cols) {
+        return fmt.Errorf("values and cols are not the same length")
+    }
 
-	for i, value := range values {
-		col := cols[i]
-		convertedVal, err := ConvertValue(value, col.Type)
-		if err != nil {
-			return fmt.Errorf("failed to convert value for column %q: %w", col.Name, err)
-		}
-		values[i] = convertedVal
-	}
-	return nil
+    for i, value := range values {
+        col := cols[i]
+        convertedVal, err := ConvertValue(value, col.Type)
+        if err != nil {
+            return fmt.Errorf("failed to convert value for column %q: %w", col.Name, err)
+        }
+        values[i] = convertedVal
+    }
+    return nil
 }
--- sources/mysql/adapter/adapter.go
+++ sources/mysql/adapter/adapter.go
@@ -1,128 +1,130 @@
 package adapter
 
 import (
-	"database/sql"
-	"fmt"
-	"log/slog"
+    "database/sql"
+    "fmt"
+    "log/slog"
 
-	"github.com/artie-labs/reader/config"
-	"github.com/artie-labs/reader/lib/debezium/converters"
-	"github.com/artie-labs/reader/lib/debezium/transformer"
-	"github.com/artie-labs/reader/lib/mysql"
-	"github.com/artie-labs/reader/lib/mysql/scanner"
-	"github.com/artie-labs/reader/lib/mysql/schema"
-	"github.com/artie-labs/reader/lib/rdbms/column"
-	"github.com/artie-labs/reader/lib/rdbms/scan"
+    "github.com/artie-labs/reader/config"
+    "github.com/artie-labs/reader/lib/debezium/converters"
+    "github.com/artie-labs/reader/lib/debezium/transformer"
+    "github.com/artie-labs/reader/lib/mysql"
+    "github.com/artie-labs/reader/lib/mysql/scanner"
+    "github.com/artie-labs/reader/lib/mysql/schema"
+    "github.com/artie-labs/reader/lib/rdbms/column"
+    "github.com/artie-labs/reader/lib/rdbms/scan"
 )
 
 const defaultErrorRetries = 10
 
 type MySQLAdapter struct {
-	db              *sql.DB
-	dbName          string
-	table           mysql.Table
-	columns         []schema.Column
-	fieldConverters []transformer.FieldConverter
-	scannerCfg      scan.ScannerConfig
+    db              *sql.DB
+    dbName          string
+    table           mysql.Table
+    columns         []schema.Column
+    fieldConverters []transformer.FieldConverter
+    scannerCfg      scan.ScannerConfig
 }
 
 func NewMySQLAdapter(db *sql.DB, dbName string, tableCfg config.MySQLTable) (MySQLAdapter, error) {
-	slog.Info("Loading metadata for table")
-	table, err := mysql.LoadTable(db, tableCfg.Name)
-	if err != nil {
-		return MySQLAdapter{}, fmt.Errorf("failed to load metadata for table %q: %w", tableCfg.Name, err)
-	}
+    slog.Info("Loading metadata for table")
+    table, err := mysql.LoadTable(db, tableCfg.Name)
+    if err != nil {
+        return MySQLAdapter{}, fmt.Errorf("failed to load metadata for table %q: %w", tableCfg.Name, err)
+    }
 
-	columns, err := column.FilterOutExcludedColumns(table.Columns, tableCfg.ExcludeColumns, table.PrimaryKeys)
-	if err != nil {
-		return MySQLAdapter{}, err
-	}
+    columns, err := column.FilterOutExcludedColumns(table.Columns, tableCfg.ExcludeColumns, table.PrimaryKeys)
+    if err != nil {
+        return MySQLAdapter{}, err
+    }
 
-	return newMySQLAdapter(db, dbName, *table, columns, tableCfg.ToScannerConfig(defaultErrorRetries))
+    return newMySQLAdapter(db, dbName, *table, columns, tableCfg.ToScannerConfig(defaultErrorRetries))
 }
 
 func newMySQLAdapter(db *sql.DB, dbName string, table mysql.Table, columns []schema.Column, scannerCfg scan.ScannerConfig) (MySQLAdapter, error) {
-	fieldConverters := make([]transformer.FieldConverter, len(columns))
-	for i, col := range columns {
-		converter, err := valueConverterForType(col.Type, col.Opts)
-		if err != nil {
-			return MySQLAdapter{}, fmt.Errorf("failed to build value converter for column %q: %w", col.Name, err)
-		}
-		fieldConverters[i] = transformer.FieldConverter{Name: col.Name, ValueConverter: converter}
-	}
+    fieldConverters := make([]transformer.FieldConverter, len(columns))
+    for i, col := range columns {
+        converter, err := valueConverterForType(col.Type, col.Opts)
+        if err != nil {
+            return MySQLAdapter{}, fmt.Errorf("failed to build value converter for column %q: %w", col.Name, err)
+        }
+        fieldConverters[i] = transformer.FieldConverter{Name: col.Name, ValueConverter: converter}
+    }
 
-	return MySQLAdapter{
-		db:              db,
-		dbName:          dbName,
-		table:           table,
-		columns:         columns,
-		fieldConverters: fieldConverters,
-		scannerCfg:      scannerCfg,
-	}, nil
+    return MySQLAdapter{
+        db:              db,
+        dbName:          dbName,
+        table:           table,
+        columns:         columns,
+        fieldConverters: fieldConverters,
+        scannerCfg:      scannerCfg,
+    }, nil
 }
 
 func (m MySQLAdapter) TableName() string {
-	return m.table.Name
+    return m.table.Name
 }
 
 func (m MySQLAdapter) TopicSuffix() string {
-	return fmt.Sprintf("%s.%s", m.dbName, m.table.Name)
+    return fmt.Sprintf("%s.%s", m.dbName, m.table.Name)
 }
 
 func (m MySQLAdapter) FieldConverters() []transformer.FieldConverter {
-	return m.fieldConverters
+    return m.fieldConverters
 }
 
 func (m MySQLAdapter) NewIterator() (transformer.RowsIterator, error) {
-	return scanner.NewScanner(m.db, m.table, m.columns, m.scannerCfg)
+    return scanner.NewScanner(m.db, m.table, m.columns, m.scannerCfg)
 }
 
 func (m MySQLAdapter) PartitionKeys() []string {
-	return m.table.PrimaryKeys
+    return m.table.PrimaryKeys
 }
 
 func valueConverterForType(d schema.DataType, opts *schema.Opts) (converters.ValueConverter, error) {
-	switch d {
-	case schema.Bit, schema.Boolean:
-		return converters.BooleanPassthrough{}, nil
-	case schema.TinyInt, schema.SmallInt:
-		return converters.Int16Passthrough{}, nil
-	case schema.MediumInt, schema.Int:
-		return converters.Int32Passthrough{}, nil
-	case schema.BigInt:
-		return converters.Int64Passthrough{}, nil
-	case schema.Float:
-		return converters.FloatPassthrough{}, nil
-	case schema.Double:
-		return converters.DoublePassthrough{}, nil
-	case schema.Decimal:
-		if opts.Scale == nil {
-			return nil, fmt.Errorf("scale is required for decimal type")
-		}
+    switch d {
+    case schema.Bit, schema.Boolean:
+        return converters.BooleanPassthrough{}, nil
+    case schema.TinyInt, schema.SmallInt:
+        return converters.Int16Passthrough{}, nil
+    case schema.MediumInt, schema.Int:
+        return converters.Int32Passthrough{}, nil
+    case schema.BigInt:
+        return converters.Int64Passthrough{}, nil
+    case schema.Float:
+        return converters.FloatPassthrough{}, nil
+    case schema.Double:
+        return converters.DoublePassthrough{}, nil
+    case schema.Decimal:
+        if opts.Scale == nil {
+            return nil, fmt.Errorf("scale is required for decimal type")
+        }
 
-		return converters.NewDecimalConverter(*opts.Scale, opts.Precision), nil
-	case schema.Char, schema.Text, schema.Varchar, schema.TinyText, schema.MediumText, schema.LongText:
-		return converters.StringPassthrough{}, nil
-	case schema.Binary, schema.Varbinary, schema.Blob:
-		return converters.BytesPassthrough{}, nil
-	case schema.Time:
-		return converters.MicroTimeConverter{}, nil
-	case schema.Date:
-		return converters.DateConverter{}, nil
-	case schema.DateTime:
-		return converters.MicroTimestampConverter{}, nil
-	case schema.Timestamp:
-		return converters.ZonedTimestampConverter{}, nil
-	case schema.Year:
-		return converters.YearConverter{}, nil
-	case schema.Enum:
-		return converters.EnumConverter{}, nil
-	case schema.Set:
-		return converters.EnumSetConverter{}, nil
-	case schema.JSON:
-		return converters.JSONConverter{}, nil
-	case schema.Point:
-		return converters.NewPointConverter(), nil
-	}
-	return nil, fmt.Errorf("unable get value converter for DataType(%d)", d)
+        return converters.NewDecimalConverter(*opts.Scale, opts.Precision), nil
+    case schema.Char, schema.Text, schema.Varchar, schema.TinyText, schema.MediumText, schema.LongText:
+        return converters.StringPassthrough{}, nil
+    case schema.Binary, schema.Varbinary, schema.Blob:
+        return converters.BytesPassthrough{}, nil
+    case schema.Time:
+        return converters.MicroTimeConverter{}, nil
+    case schema.Date:
+        return converters.DateConverter{}, nil
+    case schema.DateTime:
+        return converters.MicroTimestampConverter{}, nil
+    case schema.Timestamp:
+        return converters.ZonedTimestampConverter{}, nil
+    case schema.Year:
+        return converters.YearConverter{}, nil
+    case schema.Enum:
+        return converters.EnumConverter{}, nil
+    case schema.Set:
+        return converters.EnumSetConverter{}, nil
+    case schema.JSON:
+        return converters.JSONConverter{}, nil
+    case schema.Point:
+        return converters.NewPointConverter(), nil
+    case schema.Geometry:
+        return converters.NewGeometryConverter(), nil
+    }
+    return nil, fmt.Errorf("unable get value converter for DataType(%d)", d)
 }
