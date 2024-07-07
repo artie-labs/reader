83dab99584957f10be65170a6e2df0364ac7aa67
Robin Tang <rtang.cs@gmail.com>
2024-07-06 15:17:45 -1000

    Fix.

--- integration_tests/postgres/main.go
+++ integration_tests/postgres/main.go
@@ -508,11 +508,11 @@ const expectedPayloadTemplate = `{
 						"parameters": null
 					},
 					{
-						"type": "int32",
+						"type": "string",
 						"optional": false,
 						"default": null,
 						"field": "c_time_with_timezone",
-						"name": "io.debezium.time.Time",
+						"name": "io.debezium.time.ZonedTime",
 						"parameters": null
 					},
 					{
@@ -693,7 +693,7 @@ const expectedPayloadTemplate = `{
 			"c_serial": 1000000123,
 			"c_smallint": 32767,
 			"c_text": "QWERTYUIOP",
-			"c_time_with_timezone": 38057000,
+			"c_time_with_timezone": "10:34:17.000000Z",
 			"c_time_without_timezone": 45296000,
 			"c_timestamp_with_timezone": "2001-02-16T13:38:40Z",
 			"c_timestamp_without_timezone": 982355920000000,
