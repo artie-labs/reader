c12b5236e5b7a364928e93d11ae56f42b4f85f97
Robin Tang <rtang.cs@gmail.com>
2024-07-12 13:09:18 -0700

    Clean up.

--- lib/kafkalib/writer.go
+++ lib/kafkalib/writer.go
@@ -2,22 +2,18 @@ package kafkalib
 
 import (
 	"context"
-	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"log/slog"
 	"time"
 
-	awsCfg "github.com/aws/aws-sdk-go-v2/config"
-	"github.com/segmentio/kafka-go"
-	"github.com/segmentio/kafka-go/sasl/aws_msk_iam_v2"
-	"github.com/segmentio/kafka-go/sasl/scram"
-
 	"github.com/artie-labs/reader/config"
 	"github.com/artie-labs/reader/lib"
 	"github.com/artie-labs/reader/lib/mtr"
 	"github.com/artie-labs/transfer/lib/jitter"
+	"github.com/artie-labs/transfer/lib/kafkalib"
 	"github.com/artie-labs/transfer/lib/size"
+	"github.com/segmentio/kafka-go"
 )
 
 const (
@@ -27,60 +23,23 @@ const (
 
 func newWriter(ctx context.Context, cfg config.Kafka) (*kafka.Writer, error) {
 	slog.Info("Setting kafka bootstrap URLs", slog.Any("urls", cfg.BootstrapAddresses()))
+	kafkaConn := kafkalib.NewConnection(cfg.AwsEnabled, cfg.DisableTLS, cfg.Username, cfg.Password)
+	transport, err := kafkaConn.Transport(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create kafka transport: %w", err)
+	}
+
 	writer := &kafka.Writer{
 		Addr:                   kafka.TCP(cfg.BootstrapAddresses()...),
-		Compression:            kafka.Gzip,
-		Balancer:               &kafka.LeastBytes{},
-		WriteTimeout:           5 * time.Second,
 		AllowAutoTopicCreation: true,
+		Balancer:               &kafka.LeastBytes{},
+		Compression:            kafka.Gzip,
+		Transport:              transport,
+		WriteTimeout:           5 * time.Second,
 	}
 
 	if cfg.MaxRequestSize > 0 {
 		writer.BatchBytes = int64(cfg.MaxRequestSize)
-	}
-
-	switch cfg.Mechanism() {
-	case config.AwsMskIam:
-		// If using AWS MSK IAM, we expect this to be set in the ENV VAR
-		// (AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_REGION, or the AWS Profile should be called default.)
-		saslCfg, err := awsCfg.LoadDefaultConfig(ctx)
-		if err != nil {
-			return nil, fmt.Errorf("failed to load AWS configuration: %w", err)
-		}
-
-		transport := &kafka.Transport{
-			DialTimeout: 10 * time.Second,
-			SASL:        aws_msk_iam_v2.NewMechanism(saslCfg),
-			// Enable TLS by default
-			TLS: &tls.Config{},
-		}
-
-		if cfg.DisableTLS {
-			transport.TLS = nil
-		}
-
-		writer.Transport = transport
-	case config.ScramSha512:
-		mechanism, err := scram.Mechanism(scram.SHA512, cfg.Username, cfg.Password)
-		if err != nil {
-			return nil, fmt.Errorf("failed to create scram mechanism: %w", err)
-		}
-
-		transport := &kafka.Transport{
-			DialTimeout: 10 * time.Second,
-			SASL:        mechanism,
-			TLS:         &tls.Config{},
-		}
-
-		if cfg.DisableTLS {
-			transport.TLS = nil
-		}
-
-		writer.Transport = transport
-	case config.None:
-		// No mechanism
-	default:
-		return nil, fmt.Errorf("unsupported kafka mechanism: %s", cfg.Mechanism())
 	}
 
 	return writer, nil
