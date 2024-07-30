be5384fef9f4a1a14216ef16b76f09030e6ace0a
Robin Tang <rtang.cs@gmail.com>
2024-07-29 23:14:33 -0700

    more efficient.

--- sources/mongo/message.go
+++ sources/mongo/message.go
@@ -3,6 +3,7 @@ package mongo
 import (
 	"encoding/json"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"time"
 
 	"github.com/artie-labs/reader/config"
@@ -45,25 +46,39 @@ func ParseMessage(result bson.M, op string) (*Message, error) {
 		return nil, fmt.Errorf("failed to marshal document to JSON extended: %w", err)
 	}
 
-	var jsonExtendedMap map[string]any
-	if err = json.Unmarshal(jsonExtendedBytes, &jsonExtendedMap); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal JSON extended to map: %w", err)
-	}
-
-	pk, isOk := jsonExtendedMap["_id"]
+	bsonPk, isOk := result["_id"]
 	if !isOk {
-		return nil, fmt.Errorf("failed to get partition key, row: %v", jsonExtendedMap)
+		return nil, fmt.Errorf("failed to get partition key, row: %v", result)
 	}
 
-	pkBytes, err := json.Marshal(pk)
-	if err != nil {
-		return nil, fmt.Errorf("failed to marshal ext json: %w", err)
+	var idString string
+	switch castedPk := bsonPk.(type) {
+	case primitive.ObjectID:
+		idString = fmt.Sprintf(`{"$oid":"%s"}`, castedPk.Hex())
+	default:
+		var jsonExtendedMap map[string]any
+		if err = json.Unmarshal(jsonExtendedBytes, &jsonExtendedMap); err != nil {
+			return nil, fmt.Errorf("failed to unmarshal JSON extended to map: %w", err)
+		}
+
+		pk, isOk := jsonExtendedMap["_id"]
+		if !isOk {
+			return nil, fmt.Errorf("failed to get partition key, row: %v", jsonExtendedMap)
+		}
+
+		pkBytes, err := json.Marshal(pk)
+		if err != nil {
+			return nil, fmt.Errorf("failed to marshal ext json: %w", err)
+		}
+
+		idString = string(pkBytes)
 	}
+
 	return &Message{
 		jsonExtendedString: string(jsonExtendedBytes),
 		operation:          op,
 		pkMap: map[string]any{
-			"id": string(pkBytes),
+			"id": idString,
 		},
 	}, nil
 }
