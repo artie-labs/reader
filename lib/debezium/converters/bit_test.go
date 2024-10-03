f26ba2702655ae12cab7cdc6e4b143b996dbf49a
Robin Tang <rtang.cs@gmail.com>
2024-10-03 10:15:52 -0700

    Update test.

--- lib/debezium/converters/bit_test.go
+++ lib/debezium/converters/bit_test.go
@@ -7,8 +7,13 @@ import (
 
 func TestBitConverter_Convert(t *testing.T) {
 	{
+		// char size not specified
+		_, err := BitConverter{}.Convert("foo")
+		assert.ErrorContains(t, err, "bit converter failed: invalid char max length")
+	}
+	{
 		// char max size 1
-		converter := BitConverter{charMaxLength: 1}
+		converter := NewBitConverter(1)
 		{
 			// Invalid value - wrong type
 			_, err := converter.Convert(1234)
@@ -32,5 +37,11 @@ func TestBitConverter_Convert(t *testing.T) {
 			assert.ErrorContains(t, err, `string value "2" is not in ["0", "1"]`)
 		}
 	}
-
+	{
+		// char max size 5
+		converter := NewBitConverter(5)
+		value, err := converter.Convert("hello")
+		assert.NoError(t, err)
+		assert.Equal(t, "hello", value.(string))
+	}
 }
