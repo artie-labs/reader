3f18d5e6409d1aa14525402cf0d49834c3a391ac
Nathan <148575555+nathan-artie@users.noreply.github.com>
2024-10-18 14:56:56 -0700

    Inline `kafkalib.batched`

--- lib/kafkalib/batch.go
+++
@@ -1,8 +0,0 @@
-package kafkalib
-
-import "slices"
-
-// batched splits a slice of items into a slice of step-sized slices.
-func batched[T any](items []T, step int) [][]T {
-	return slices.Collect(slices.Chunk(items, max(step, 1)))
-}
