9ff91887bc6067133137b34e2b34ffb64ab2a6fc
Robin Tang <rtang.cs@gmail.com>
2024-11-07 10:54:15 -0800

    Checkpoint.

--- integration_tests/mssql/main.go
+++ integration_tests/mssql/main.go
@@ -11,6 +11,7 @@ import (
 	"os"
 
 	"github.com/lmittmann/tint"
+	_ "github.com/microsoft/go-mssqldb"
 
 	"github.com/artie-labs/reader/config"
 	"github.com/artie-labs/reader/integration_tests/utils"
@@ -33,7 +34,7 @@ func main() {
 		Database: "master",
 	}
 
-	db, err := sql.Open("sqlserver", mssqlCfg.ToDSN())
+	db, err := sql.Open("mssql", mssqlCfg.ToDSN())
 	if err != nil {
 		logger.Fatal("Could not connect to SQL Server", slog.Any("err", err))
 	}
@@ -42,15 +43,12 @@ func main() {
 		logger.Fatal("Types test failed", slog.Any("err", err))
 	}
 
-	if err = testScan(db, mssqlCfg.Database); err != nil {
-		logger.Fatal("Scan test failed", slog.Any("err", err))
-	}
-
 	slog.Info("Test succeeded 😎")
 }
 
 func readTable(db *sql.DB, dbName, tableName string, batchSize int) ([]lib.RawMessage, error) {
 	tableCfg := config.MSSQLTable{
+		Schema:    "dbo",
 		Name:      tableName,
 		BatchSize: uint(batchSize),
 	}
@@ -65,19 +63,29 @@ func readTable(db *sql.DB, dbName, tableName string, batchSize int) ([]lib.RawMe
 
 const testTypesCreateTableQuery = `
 CREATE TABLE %s (
-	pk INT PRIMARY KEY NOT NULL,
-	c_int INT,
-	c_varchar VARCHAR(100),
-	c_date DATE
+	pk INTEGER PRIMARY KEY NOT NULL,
+	c_bit BIT,
+	c_bytes VARBINARY,
+	c_int16 SMALLINT,
+	c_int32 INT,
+	c_int64 BIGINT
 )
 `
 
 const testTypesInsertQuery = `
 INSERT INTO %s VALUES (
+	-- pk
 	1,
+	-- c_bit
+	1,
+	-- c_bytes
+	10101,
+	-- c_int16
 	123,
-	'Test',
-	'2020-01-01'
+	-- c_int32
+	1234,
+	-- c_int64
+	1235
 )
 `
 
@@ -146,8 +154,8 @@ const expectedPayloadTemplate = `{
 }`
 
 func testTypes(db *sql.DB, dbName string) error {
-	tempTableName, dropTableFunc := utils.CreateTemporaryTable(db, testTypesCreateTableQuery)
-	defer dropTableFunc()
+	tempTableName, _ := utils.CreateTemporaryTable(db, testTypesCreateTableQuery)
+	//defer dropTableFunc()
 
 	// Check reading an empty table
 	_, err := readTable(db, dbName, tempTableName, 100)
@@ -185,63 +193,6 @@ func testTypes(db *sql.DB, dbName string) error {
 	expectedPayload := fmt.Sprintf(expectedPayloadTemplate, utils.GetEvent(row).Payload.Source.TsMs, tempTableName)
 	if utils.CheckDifference("payload", expectedPayload, string(valueBytes)) {
 		return fmt.Errorf("payload does not match")
-	}
-
-	return nil
-}
-
-const testScanCreateTableQuery = `
-CREATE TABLE %s (
-	c_int_pk INT NOT NULL,
-	c_boolean_pk BIT NOT NULL,
-	c_text_pk VARCHAR(2) NOT NULL,
-	c_text_value TEXT,
-	PRIMARY KEY(c_int_pk, c_boolean_pk, c_text_pk)
-)
-`
-
-const testScanInsertQuery = `
-INSERT INTO %s VALUES
-(1, 0, 'A', 'row 1'),
-(2, 1, 'B', 'row 2')
-`
-
-func testScan(db *sql.DB, dbName string) error {
-	tempTableName, dropTableFunc := utils.CreateTemporaryTable(db, testScanCreateTableQuery)
-	defer dropTableFunc()
-
-	slog.Info("Inserting data...")
-	if _, err := db.Exec(fmt.Sprintf(testScanInsertQuery, tempTableName)); err != nil {
-		return fmt.Errorf("unable to insert data: %w", err)
-	}
-
-	expectedPartitionKeys := []map[string]any{
-		{"c_int_pk": int64(1), "c_boolean_pk": int64(0), "c_text_pk": "A"},
-		{"c_int_pk": int64(2), "c_boolean_pk": int64(1), "c_text_pk": "B"},
-	}
-	expectedValues := []string{
-		"row 1",
-		"row 2",
-	}
-
-	for _, batchSize := range []int{1, 2} {
-		slog.Info(fmt.Sprintf("Testing scan with batch size of %d...", batchSize))
-		rows, err := readTable(db, dbName, tempTableName, batchSize)
-		if err != nil {
-			return err
-		}
-		if len(rows) != len(expectedPartitionKeys) {
-			return fmt.Errorf("expected %d rows, got %d, batch size %d", len(expectedPartitionKeys), len(rows), batchSize)
-		}
-		for i, row := range rows {
-			if !maps.Equal(row.PartitionKey(), expectedPartitionKeys[i]) {
-				return fmt.Errorf("partition keys are different for row %d, batch size %d, %v != %v", i, batchSize, row.PartitionKey(), expectedPartitionKeys[i])
-			}
-			textValue := utils.GetEvent(row).Payload.After["c_text_value"]
-			if textValue != expectedValues[i] {
-				return fmt.Errorf("row values are different for row %d, batch size %d, %v != %v", i, batchSize, textValue, expectedValues[i])
-			}
-		}
 	}
 
 	return nil
