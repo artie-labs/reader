80a955666b06184f397511ee139bb5733e04551a
Robin Tang <rtang.cs@gmail.com>
2024-11-15 13:41:52 -0800

    [MySQL] Streaming support Part 2 (#548)

--- sources/mysql/streaming.go
+++ sources/mysql/streaming.go
@@ -2,34 +2,76 @@ package mysql
 
 import (
 	"context"
+	"log/slog"
 
+	"github.com/artie-labs/transfer/lib/typing"
 	"github.com/go-mysql-org/go-mysql/replication"
 
 	"github.com/artie-labs/reader/config"
+	"github.com/artie-labs/reader/lib/storage/persistedmap"
+	"github.com/artie-labs/reader/sources/mysql/streaming"
 	"github.com/artie-labs/reader/writers"
 )
 
+const offsetKey = "offset"
+
 type Streaming struct {
-	syncer *replication.BinlogSyncer
+	storage           *persistedmap.PersistedMap
+	syncer            *replication.BinlogSyncer
+	position          streaming.Position
+	includedTablesMap map[string]bool
+}
+
+func (s Streaming) shouldProcessTable(tableName string) bool {
+	_, isOk := s.includedTablesMap[tableName]
+	return isOk
 }
 
 func (s Streaming) Close() error {
+	s.syncer.Close()
 	return nil
 }
 
 func buildStreamingConfig(cfg config.MySQL) (Streaming, error) {
-	syncerConfig := replication.BinlogSyncerConfig{
-		ServerID: cfg.StreamingSettings.ServerID,
-		Flavor:   "mysql",
-		Host:     cfg.Host,
-		Port:     uint16(cfg.Port),
-		User:     cfg.Username,
-		Password: cfg.Password,
+	includedTablesMap := make(map[string]bool)
+	for _, table := range cfg.Tables {
+		includedTablesMap[table.Name] = true
 	}
 
-	return Streaming{syncer: replication.NewBinlogSyncer(syncerConfig)}, nil
+	streamer := Streaming{
+		storage: persistedmap.NewPersistedMap(cfg.StreamingSettings.OffsetFile),
+		syncer: replication.NewBinlogSyncer(
+			replication.BinlogSyncerConfig{
+				ServerID: cfg.StreamingSettings.ServerID,
+				Flavor:   "mysql",
+				Host:     cfg.Host,
+				Port:     uint16(cfg.Port),
+				User:     cfg.Username,
+				Password: cfg.Password,
+			},
+		),
+		includedTablesMap: includedTablesMap,
+	}
+
+	value, isOk := streamer.storage.Get(offsetKey)
+	if isOk {
+		pos, err := typing.AssertType[streaming.Position](value)
+		if err != nil {
+			return Streaming{}, err
+		}
+
+		slog.Info("Loaded offsets", slog.String("offset", pos.String()))
+		streamer.position = pos
+	}
+
+	return streamer, nil
 }
 
 func (s Streaming) Run(ctx context.Context, writer writers.Writer) error {
+	_, err := s.syncer.StartSync(s.position.ToMySQLPosition())
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
