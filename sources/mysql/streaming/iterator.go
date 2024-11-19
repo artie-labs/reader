c14a9fafa2d9a79c798c093544d2eed915fa3a00
Robin Tang <rtang.cs@gmail.com>
2024-11-18 20:03:07 -0800

    Clean up.

--- sources/mysql/streaming/iterator.go
+++ sources/mysql/streaming/iterator.go
@@ -1,21 +1,23 @@
 package streaming
 
 import (
+	"context"
 	"fmt"
 	"github.com/artie-labs/reader/config"
 	"github.com/artie-labs/reader/lib"
-	"github.com/artie-labs/reader/lib/iterator"
 	"github.com/artie-labs/reader/lib/maputil"
 	"github.com/artie-labs/reader/lib/storage/persistedmap"
 	"github.com/artie-labs/reader/sources/mysql/adapter"
 	"github.com/artie-labs/transfer/lib/typing"
 	"github.com/go-mysql-org/go-mysql/replication"
 	"log/slog"
+	"time"
 )
 
 const offsetKey = "offset"
 
 type Iterator struct {
+	batchSize             int32
 	offsets               *persistedmap.PersistedMap
 	streamer              *replication.BinlogStreamer
 	position              Position
@@ -26,10 +28,66 @@ type Iterator struct {
 }
 
 func (i *Iterator) Next() ([]lib.RawMessage, error) {
-	//TODO implement me
-	panic("implement me")
-}
+	fmt.Println("next here")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
+	var rawMsgs []lib.RawMessage
+	for i.batchSize > int32(len(rawMsgs)) {
+		select {
+		case <-ctx.Done():
+			fmt.Println("returning")
+			return rawMsgs, nil
+		default:
+			event, err := i.streamer.GetEvent(ctx)
+			if err != nil {
+				return nil, fmt.Errorf("failed to get binlog event: %w", err)
+			}
+
+			// Update the position
+			i.position.Pos = event.Header.LogPos
+
+			switch event.Header.EventType {
+			case replication.ROTATE_EVENT:
+				rotate, err := typing.AssertType[*replication.RotateEvent](event.Event)
+				if err != nil {
+					return nil, fmt.Errorf("failed to assert rotate event: %w", err)
+				}
+				i.position = Position{File: string(rotate.NextLogName)}
+			case replication.QUERY_EVENT:
+				query, err := typing.AssertType[*replication.QueryEvent](event.Event)
+				if err != nil {
+					return nil, fmt.Errorf("failed to assert query event: %w", err)
+				}
+
+				fmt.Println("query", query)
+				// TODO: Process the DDL event
+			case replication.WRITE_ROWS_EVENTv2, replication.UPDATE_ROWS_EVENTv2, replication.DELETE_ROWS_EVENTv2:
+				rowsEvent, ok := event.Event.(*replication.RowsEvent)
+				if !ok {
+					return nil, fmt.Errorf("unable to cast event to replication.RowsEvent")
+				}
+
+				if !i.shouldProcessTable(rowsEvent.Table.Table) {
+					continue
+				}
+
+				// TODO: Process the event and convert it to lib.RawMessage
+				// rawMessage := processRowsEvent(rowsEvent)
+				// rawMsgs = append(rawMsgs, rawMessage)
+			default:
+				slog.Info("Skipping event", slog.Any("event", event.Header.EventType))
+			}
+		}
+	}
+
+	if len(rawMsgs) == 0 {
+		// If there are no messages, let's sleep a bit before we try again
+		time.Sleep(2 * time.Second)
+	}
+
+	return rawMsgs, nil
+}
 func (i *Iterator) shouldProcessTable(tableName []byte) bool {
 	_, isOk := i.includedTablesAdapter[string(tableName)]
 	return isOk
@@ -44,7 +102,7 @@ func (i *Iterator) CommitOffset() {
 	slog.Info("Committing offset", slog.Any("position", i.position))
 }
 
-func BuildStreamingIterator(cfg config.MySQL) (iterator.StreamingIterator[[]lib.RawMessage], error) {
+func BuildStreamingIterator(cfg config.MySQL) (*Iterator, error) {
 	var pos Position
 	offsets := persistedmap.NewPersistedMap(cfg.StreamingSettings.OffsetFile)
 	value, isOk := offsets.Get(offsetKey)
@@ -79,7 +137,7 @@ func BuildStreamingIterator(cfg config.MySQL) (iterator.StreamingIterator[[]lib.
 		return nil, fmt.Errorf("failed to start sync: %w", err)
 	}
 
-	return Iterator{
+	return &Iterator{
 		position:              pos,
 		streamer:              streamer,
 		includedTablesAdapter: includedTablesAdapter,
