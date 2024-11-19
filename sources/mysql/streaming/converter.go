064f925bee435f7c4d9cf95ad37c2349382af71e
Robin Tang <rtang.cs@gmail.com>
2024-11-18 19:14:13 -0800

    Clean up.

---
+++ sources/mysql/streaming/converter.go
@@ -0,0 +1,19 @@
+package streaming
+
+import (
+	"fmt"
+	"github.com/go-mysql-org/go-mysql/replication"
+)
+
+func convertHeaderToOperation(evtType replication.EventType) (string, error) {
+	switch evtType {
+	case replication.WRITE_ROWS_EVENTv2:
+		return "c", nil
+	case replication.UPDATE_ROWS_EVENTv2:
+		return "u", nil
+	case replication.DELETE_ROWS_EVENTv2:
+		return "d", nil
+	default:
+		return "", fmt.Errorf("unexpected event type: %T", evtType)
+	}
+}
