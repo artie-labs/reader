015e2f76b50b94bfe57015e3a3d1fe95139beddf
Robin Tang <rtang.cs@gmail.com>
2024-11-20 09:34:03 -0800

    Clean up.

--- lib/debezium/transformer/light_transformer.go
+++ lib/debezium/transformer/light_transformer.go
@@ -30,39 +30,48 @@ func NewLightDebeziumTransformer(tableName string, fieldConverters []FieldConver
 }
 
 func (l LightDebeziumTransformer) BuildEventPayload(beforeRow Row, afterRow Row, op string, ts time.Time) (util.SchemaEventPayload, error) {
-	before, err := convertRow(l.valueConverters, beforeRow)
-	if err != nil {
-		return util.SchemaEventPayload{}, fmt.Errorf("failed to convert before row: %w", err)
+	schema := debezium.Schema{FieldsObject: []debezium.FieldsObject{}}
+	payload := util.Payload{
+		Source: util.Source{
+			Table: l.tableName,
+			TsMs:  ts.UnixMilli(),
+		},
+		Operation: op,
 	}
 
-	after, err := convertRow(l.valueConverters, afterRow)
-	if err != nil {
-		return util.SchemaEventPayload{}, fmt.Errorf("failed to convert after row: %w", err)
+	if beforeRow != nil {
+		before, err := convertRow(l.valueConverters, beforeRow)
+		if err != nil {
+			return util.SchemaEventPayload{}, fmt.Errorf("failed to convert before row: %w", err)
+		}
+
+		schema.FieldsObject = append(schema.FieldsObject,
+			debezium.FieldsObject{
+				Fields:     l.fields,
+				Optional:   false,
+				FieldLabel: debezium.Before,
+			},
+		)
+
+		payload.Before = before
 	}
 
-	return util.SchemaEventPayload{
-		Schema: debezium.Schema{
-			FieldsObject: []debezium.FieldsObject{
-				{
-					Fields:     l.fields,
-					Optional:   false,
-					FieldLabel: debezium.Before,
-				},
-				{
-					Fields:     l.fields,
-					Optional:   false,
-					FieldLabel: debezium.After,
-				},
+	if afterRow != nil {
+		after, err := convertRow(l.valueConverters, afterRow)
+		if err != nil {
+			return util.SchemaEventPayload{}, fmt.Errorf("failed to convert after row: %w", err)
+		}
+
+		schema.FieldsObject = append(schema.FieldsObject,
+			debezium.FieldsObject{
+				Fields:     l.fields,
+				Optional:   false,
+				FieldLabel: debezium.After,
 			},
-		},
-		Payload: util.Payload{
-			Before: before,
-			After:  after,
-			Source: util.Source{
-				Table: l.tableName,
-				TsMs:  ts.UnixMilli(),
-			},
-			Operation: op,
-		},
-	}, nil
+		)
+
+		payload.After = after
+	}
+
+	return util.SchemaEventPayload{Schema: schema, Payload: payload}, nil
 }
