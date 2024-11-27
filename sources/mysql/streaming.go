8b686286213aebe51907c771e421d978b19d6ba5
Robin Tang <rtang.cs@gmail.com>
2024-11-27 09:33:10 -0800

    Update.

--- sources/mysql/streaming.go
+++ sources/mysql/streaming.go
@@ -2,70 +2,31 @@ package mysql
 
 import (
 	"context"
-	"log/slog"
-
-	"github.com/go-mysql-org/go-mysql/replication"
-
 	"github.com/artie-labs/reader/config"
-	"github.com/artie-labs/reader/lib/storage/persistedmap"
 	"github.com/artie-labs/reader/sources/mysql/streaming"
 	"github.com/artie-labs/reader/writers"
 )
 
-const offsetKey = "offset"
-
 type Streaming struct {
-	offsets           *persistedmap.PersistedMap[streaming.Position]
-	syncer            *replication.BinlogSyncer
-	position          streaming.Position
-	includedTablesMap map[string]bool
-}
-
-func (s Streaming) shouldProcessTable(tableName string) bool {
-	_, isOk := s.includedTablesMap[tableName]
-	return isOk
-}
-
-func (s Streaming) Close() error {
-	s.syncer.Close()
-	return nil
+	iterator *streaming.Iterator
 }
 
 func buildStreamingConfig(cfg config.MySQL) (Streaming, error) {
-	includedTablesMap := make(map[string]bool)
-	for _, table := range cfg.Tables {
-		includedTablesMap[table.Name] = true
+	iter, err := streaming.BuildStreamingIterator(cfg)
+	if err != nil {
+		return Streaming{}, err
 	}
 
-	streamer := Streaming{
-		offsets: persistedmap.NewPersistedMap[streaming.Position](cfg.StreamingSettings.OffsetFile),
-		syncer: replication.NewBinlogSyncer(
-			replication.BinlogSyncerConfig{
-				ServerID: cfg.StreamingSettings.ServerID,
-				Flavor:   "mysql",
-				Host:     cfg.Host,
-				Port:     uint16(cfg.Port),
-				User:     cfg.Username,
-				Password: cfg.Password,
-			},
-		),
-		includedTablesMap: includedTablesMap,
-	}
+	return Streaming{
+		iterator: &iter,
+	}, nil
+}
 
-	position, isOk := streamer.offsets.Get(offsetKey)
-	if isOk {
-		slog.Info("Loaded offsets", slog.String("offset", position.String()))
-		streamer.position = position
-	}
-
-	return streamer, nil
+func (s Streaming) Close() error {
+	return s.iterator.Close()
 }
 
 func (s Streaming) Run(ctx context.Context, writer writers.Writer) error {
-	_, err := s.syncer.StartSync(s.position.ToMySQLPosition())
-	if err != nil {
-		return err
-	}
-
-	return nil
+	_, err := writer.Write(ctx, s.iterator)
+	return err
 }
---
+++ sources/mysql/streaming/iterator.go
@@ -0,0 +1,31 @@
+package streaming
+
+import (
+	"github.com/artie-labs/reader/config"
+	"github.com/artie-labs/reader/lib"
+)
+
+func BuildStreamingIterator(_ config.MySQL) (Iterator, error) {
+	// TODO
+	return Iterator{}, nil
+}
+
+func (i *Iterator) HasNext() bool {
+	// TODO
+	return true
+}
+
+func (i *Iterator) CommitOffset() {
+	// TODO
+	return
+}
+
+func (i *Iterator) Close() error {
+	// TODO
+	return nil
+}
+
+func (i *Iterator) Next() ([]lib.RawMessage, error) {
+	// TODO
+	return nil, nil
+}
