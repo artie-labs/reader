a54d3708bf59a23d4ecced2c6a361ce0ae5f80ad
Robin Tang <rtang.cs@gmail.com>
2024-12-02 15:21:49 -0800

    Adding more Iterator logic.

--- sources/mysql/streaming/iterator.go
+++ sources/mysql/streaming/iterator.go
@@ -1,26 +1,60 @@
 package streaming
 
 import (
+	"fmt"
 	"github.com/artie-labs/reader/config"
 	"github.com/artie-labs/reader/lib"
+	"github.com/artie-labs/reader/lib/storage/persistedmap"
+	"github.com/go-mysql-org/go-mysql/replication"
+	"log/slog"
 )
 
-func BuildStreamingIterator(_ config.MySQL) (Iterator, error) {
-	// TODO
-	return Iterator{}, nil
+const offsetKey = "offset"
+
+func BuildStreamingIterator(cfg config.MySQL) (Iterator, error) {
+	var pos Position
+	offsets := persistedmap.NewPersistedMap[Position](cfg.StreamingSettings.OffsetFile)
+	if _pos, isOk := offsets.Get(offsetKey); isOk {
+		slog.Info("Found offsets", slog.String("offset", _pos.String()))
+		pos = _pos
+	}
+
+	syncer := replication.NewBinlogSyncer(
+		replication.BinlogSyncerConfig{
+			ServerID: cfg.StreamingSettings.ServerID,
+			Flavor:   "mysql",
+			Host:     cfg.Host,
+			Port:     uint16(cfg.Port),
+			User:     cfg.Username,
+			Password: cfg.Password,
+		},
+	)
+
+	streamer, err := syncer.StartSync(pos.ToMySQLPosition())
+	if err != nil {
+		return Iterator{}, fmt.Errorf("failed to start sync: %w", err)
+	}
+
+	return Iterator{
+		batchSize: cfg.GetStreamingBatchSize(),
+		position:  pos,
+		syncer:    syncer,
+		streamer:  streamer,
+		offsets:   offsets,
+	}, nil
 }
 
 func (i *Iterator) HasNext() bool {
-	// TODO
 	return true
 }
 
 func (i *Iterator) CommitOffset() {
-	// TODO
+	slog.Info("Committing offset", slog.String("position", i.position.String()))
+	i.offsets.Set(offsetKey, i.position)
 }
 
 func (i *Iterator) Close() error {
-	// TODO
+	i.syncer.Close()
 	return nil
 }
 
--- sources/mysql/streaming/types.go
+++ sources/mysql/streaming/types.go
@@ -2,10 +2,19 @@ package streaming
 
 import (
 	"fmt"
+	"github.com/artie-labs/reader/lib/storage/persistedmap"
+
 	"github.com/go-mysql-org/go-mysql/mysql"
+	"github.com/go-mysql-org/go-mysql/replication"
 )
 
-type Iterator struct{}
+type Iterator struct {
+	batchSize int32
+	position  Position
+	offsets   *persistedmap.PersistedMap[Position]
+	streamer  *replication.BinlogStreamer
+	syncer    *replication.BinlogSyncer
+}
 
 type Position struct {
 	File string `yaml:"file"`
