48411fb1cda9fda6c43814096beff40fdc8b87d3
Robin Tang <rtang.cs@gmail.com>
2024-12-02 16:47:06 -0800

    PersistList.

---
+++ lib/storage/persistedlist/persistedlist.go
@@ -0,0 +1,77 @@
+package persistedlist
+
+import (
+	"bufio"
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"log/slog"
+	"os"
+
+	"github.com/artie-labs/reader/lib/logger"
+)
+
+type PersistedList[T any] struct {
+	filePath string
+}
+
+func NewPersistedList[T any](filePath string) *PersistedList[T] {
+	return &PersistedList[T]{
+		filePath: filePath,
+	}
+}
+
+func (p PersistedList[T]) Push(item T) error {
+	// If the file doesn't exist, create it
+	file, err := os.OpenFile(p.filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("failed to open file")
+	}
+
+	bytes, err := yaml.Marshal(item)
+	if err != nil {
+		return fmt.Errorf("failed to marshal data")
+	}
+
+	if _, err := file.Write(bytes); err != nil {
+		return fmt.Errorf("failed to write to file")
+	}
+
+	return nil
+}
+
+// GetData - This is a separate function since we don't need to keep the entire list in memory
+func (p PersistedList[T]) GetData() []T {
+	data, err := loadFromFile[T](p.filePath)
+	if err != nil {
+		logger.Panic("Failed to load persisted map from filepath", slog.Any("err", err))
+	}
+
+	return data
+}
+
+func loadFromFile[T any](filePath string) ([]T, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, nil
+		}
+
+		return nil, fmt.Errorf("failed to open file: %w", err)
+	}
+
+	defer file.Close()
+
+	// Read each line, unmarshal it, and append it to the data slice
+	var data []T
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		var t T
+		if err = yaml.Unmarshal(scanner.Bytes(), &t); err != nil {
+			return nil, fmt.Errorf("failed to unmarshal data: %w", err)
+		}
+
+		data = append(data, t)
+	}
+
+	return data, nil
+}
---
+++ sources/mysql/streaming/ddl.go
@@ -0,0 +1,14 @@
+package streaming
+
+import "github.com/go-mysql-org/go-mysql/replication"
+
+type Schema struct {
+
+}
+
+
+func (i *Iterator) processDDL(event *replication.QueryEvent) error {
+
+
+
+}
