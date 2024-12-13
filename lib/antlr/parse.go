b1e5e4c01333ac248a5ab5372eef87c65bf8d055
Robin Tang <rtang.cs@gmail.com>
2024-12-13 10:27:57 -0800

    Update.

--- lib/antlr/parse.go
+++ lib/antlr/parse.go
@@ -77,7 +77,10 @@ func visit(tree antlr.Tree) ([]Event, error) {
 		*generated.TruncateTableContext,
 		*generated.AdministrationStatementContext,
 		*generated.CreateDatabaseContext,
-		*antlr.TerminalNodeImpl:
+		*antlr.TerminalNodeImpl,
+		// Ignoring *generated.DmlStatementContext since it can pick up
+		// INSERT INTO mysql.rds_heartbeat2(id, value)
+		*generated.DmlStatementContext:
 		return nil, nil
 	default:
 		return nil, newParseError(fmt.Errorf("unsupported context type: %T", ctx))
