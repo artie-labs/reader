cd9d8f0ec311fc393debce541ef493800c6dc756
Robin Tang <rtang.cs@gmail.com>
2024-12-13 16:55:24 -0800

    [MySQL] SchemaAdapter missed dbName (#596)

--- sources/mysql/streaming/ddl/ddl.go
+++ sources/mysql/streaming/ddl/ddl.go
@@ -24,6 +24,7 @@ func NewSchemaAdapter(cfg config.MySQL) SchemaAdapter {
 	return SchemaAdapter{
 		adapters:    make(map[string]TableAdapter),
 		tableCfgMap: tableCfgMap,
+		dbName:      cfg.Database,
 	}
 }
 
