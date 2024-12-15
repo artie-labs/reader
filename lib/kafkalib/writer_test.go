b0b93f249d4354cfc81769da2b9b7ab59a55e2e0
Robin Tang <rtang.cs@gmail.com>
2024-12-15 14:22:17 -0800

    Clean up.

--- lib/kafkalib/writer_test.go
+++ lib/kafkalib/writer_test.go
@@ -1,19 +1,22 @@
 package kafkalib
 
 import (
-	"github.com/artie-labs/transfer/lib/debezium"
 	"testing"
 
-	"github.com/artie-labs/reader/lib"
 	"github.com/artie-labs/transfer/lib/cdc/util"
+	"github.com/artie-labs/transfer/lib/debezium"
+	"github.com/artie-labs/transfer/lib/kafkalib"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/artie-labs/reader/lib"
 )
 
 func TestNewMessage(t *testing.T) {
+	pkMap := map[string]any{"key": "value"}
 	rawMessage := lib.NewRawMessage(
 		"topic-suffix",
 		debezium.FieldsObject{},
-		map[string]any{"key": "value"},
+		pkMap,
 		&util.SchemaEventPayload{
 			Payload: util.Payload{
 				After: map[string]any{"a": "b"},
@@ -31,4 +34,9 @@ func TestNewMessage(t *testing.T) {
 	assert.Equal(t, "topic-prefix.topic-suffix", msg.Topic)
 	assert.Equal(t, `{"schema":{"type":"","fields":null,"optional":false,"field":""},"payload":{"key":"value"}}`, string(msg.MessageKey))
 	assert.Equal(t, `{"schema":{"type":"","fields":null},"payload":{"before":null,"after":{"a":"b"},"source":{"connector":"","ts_ms":1000,"db":"","table":"table"},"op":"c"}}`, string(msg.MessageValue))
+
+	// Parse this using JSON
+	returnedPkMap, err := debezium.ParsePartitionKey(msg.MessageKey, kafkalib.JSONKeyFmt)
+	assert.NoError(t, err)
+	assert.Equal(t, pkMap, returnedPkMap)
 }
