90628dc68d5d6c6864bb8b17207fe9bb937205a5
Robin Tang <rtang.cs@gmail.com>
2024-12-17 10:31:03 -0800

    Fix tests.

--- lib/mysql/schema/schema_test.go
+++ lib/mysql/schema/schema_test.go
@@ -15,15 +15,15 @@ func TestParseColumnDataType(t *testing.T) {
 	{
 		// Invalid
 		{
-			_, _, err := ParseColumnDataType("int(10 unsigned")
+			_, _, err := ParseColumnDataType("int(10 unsigned", nil)
 			assert.ErrorContains(t, err, `malformed data type: "int(10 unsigned"`)
 		}
 		{
-			_, _, err := ParseColumnDataType("foo")
+			_, _, err := ParseColumnDataType("foo", nil)
 			assert.ErrorContains(t, err, `unknown data type: "foo"`)
 		}
 		{
-			_, _, err := ParseColumnDataType("varchar(")
+			_, _, err := ParseColumnDataType("varchar(", nil)
 			assert.ErrorContains(t, err, `malformed data type: "varchar("`)
 		}
 	}
@@ -31,50 +31,50 @@ func TestParseColumnDataType(t *testing.T) {
 		// Integers
 		{
 			// int
-			dataType, _, err := ParseColumnDataType("int")
+			dataType, _, err := ParseColumnDataType("int", nil)
 			assert.NoError(t, err)
 			assert.Equal(t, Int, dataType)
 		}
 		{
 			// int
-			dataType, _, err := ParseColumnDataType("INTEGER")
+			dataType, _, err := ParseColumnDataType("INTEGER", nil)
 			assert.NoError(t, err)
 			assert.Equal(t, Int, dataType)
 		}
 		{
 			// int unsigned
-			dataType, _, err := ParseColumnDataType("int unsigned")
+			dataType, _, err := ParseColumnDataType("int unsigned", nil)
 			assert.NoError(t, err)
 			assert.Equal(t, BigInt, dataType)
 		}
 		{
 			// int(10) unsigned
-			dataType, _, err := ParseColumnDataType("int(10) unsigned")
+			dataType, _, err := ParseColumnDataType("int(10) unsigned", nil)
 			assert.NoError(t, err)
 			assert.Equal(t, BigInt, dataType)
 		}
 		{
 			// tinyint
-			dataType, _, err := ParseColumnDataType("tinyint")
+			dataType, _, err := ParseColumnDataType("tinyint", nil)
 			assert.NoError(t, err)
 			assert.Equal(t, TinyInt, dataType)
 		}
 		{
 			// tinyint unsigned
-			dataType, _, err := ParseColumnDataType("tinyint unsigned")
+			dataType, _, err := ParseColumnDataType("tinyint unsigned", nil)
 			assert.NoError(t, err)
 			assert.Equal(t, SmallInt, dataType)
 		}
 		{
 			// mediumint unsigned
-			dataType, _, err := ParseColumnDataType("mediumint unsigned")
+			dataType, _, err := ParseColumnDataType("mediumint unsigned", nil)
 			assert.NoError(t, err)
 			assert.Equal(t, Int, dataType)
 		}
 	}
 	{
 		// tinyint(1) should still be an integer
-		dataType, _, err := ParseColumnDataType("tinyint(1)")
+		dataType, _, err := ParseColumnDataType("tinyint(1)", nil)
 		assert.NoError(t, err)
 		assert.Equal(t, TinyInt, dataType)
 	}
@@ -82,21 +82,21 @@ func TestParseColumnDataType(t *testing.T) {
 		// String
 		{
 			// VARCHAR
-			dataType, opts, err := ParseColumnDataType("varchar(255)")
+			dataType, opts, err := ParseColumnDataType("varchar(255)", nil)
 			assert.NoError(t, err)
 			assert.Equal(t, Varchar, dataType)
 			assert.Equal(t, &Opts{Size: typing.ToPtr(255)}, opts)
 		}
 		{
 			// VARCHAR with collation
-			dataType, opts, err := ParseColumnDataType(`varchar(255) COLLATE utf8mb3_unicode_ci`)
+			dataType, opts, err := ParseColumnDataType(`varchar(255) COLLATE utf8mb3_unicode_ci`, nil)
 			assert.NoError(t, err)
 			assert.Equal(t, Varchar, dataType)
 			assert.Equal(t, &Opts{Size: typing.ToPtr(255)}, opts)
 		}
 		{
 			// VARCHAR with collation and character set
-			dataType, opts, err := ParseColumnDataType(`varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci`)
+			dataType, opts, err := ParseColumnDataType(`varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci`, nil)
 			assert.NoError(t, err)
 			assert.Equal(t, Varchar, dataType)
 			assert.Equal(t, &Opts{Size: typing.ToPtr(255)}, opts)
@@ -104,7 +104,7 @@ func TestParseColumnDataType(t *testing.T) {
 	}
 	{
 		// Decimal
-		dataType, opts, err := ParseColumnDataType("decimal(5,2)")
+		dataType, opts, err := ParseColumnDataType("decimal(5,2)", nil)
 		assert.NoError(t, err)
 		assert.Equal(t, Decimal, dataType)
 		assert.Equal(t, &Opts{Precision: typing.ToPtr(5), Scale: typing.ToPtr(uint16(2))}, opts)
@@ -113,21 +113,21 @@ func TestParseColumnDataType(t *testing.T) {
 		// Enum
 		{
 			// No need to escape
-			dataType, opts, err := ParseColumnDataType("enum('a','b','c')")
+			dataType, opts, err := ParseColumnDataType("enum('a','b','c')", nil)
 			assert.NoError(t, err)
 			assert.Equal(t, Enum, dataType)
 			assert.Equal(t, &Opts{EnumValues: []string{"a", "b", "c"}}, opts)
 		}
 		{
 			// No need to escape, testing for capitalization
-			dataType, opts, err := ParseColumnDataType("ENUM('A','B','C')")
+			dataType, opts, err := ParseColumnDataType("ENUM('A','B','C')", nil)
 			assert.NoError(t, err)
 			assert.Equal(t, Enum, dataType)
 			assert.Equal(t, &Opts{EnumValues: []string{"A", "B", "C"}}, opts)
 		}
 		{
 			// Need to escape
-			dataType, opts, err := ParseColumnDataType(`enum('newline\n','tab	','backslash\\','quote''s')`)
+			dataType, opts, err := ParseColumnDataType(`enum('newline\n','tab	','backslash\\','quote''s')`, nil)
 			assert.NoError(t, err)
 			assert.Equal(t, Enum, dataType)
 			assert.Equal(t, &Opts{EnumValues: []string{"newline\\n", "tab\t", "backslash\\\\", "quote's"}}, opts)
@@ -136,7 +136,7 @@ func TestParseColumnDataType(t *testing.T) {
 		}
 		{
 			// Need to escape another one
-			dataType, opts, err := ParseColumnDataType("ENUM('active','inactive','on hold','approved by ''manager''','needs \\\\review')")
+			dataType, opts, err := ParseColumnDataType("ENUM('active','inactive','on hold','approved by ''manager''','needs \\\\review')", nil)
 			assert.NoError(t, err)
 			assert.Equal(t, Enum, dataType)
 			assert.Equal(t, &Opts{EnumValues: []string{"active", "inactive", "on hold", "approved by 'manager'", "needs \\\\review"}}, opts)
@@ -147,14 +147,14 @@ func TestParseColumnDataType(t *testing.T) {
 		// Set
 		{
 			// No need to escape
-			dataType, opts, err := ParseColumnDataType("set('a','b','c')")
+			dataType, opts, err := ParseColumnDataType("set('a','b','c')", nil)
 			assert.NoError(t, err)
 			assert.Equal(t, Set, dataType)
 			assert.Equal(t, &Opts{EnumValues: []string{"a", "b", "c"}}, opts)
 		}
 		{
 			// No need to escape, testing for capitalization
-			dataType, opts, err := ParseColumnDataType("SET('A','B','C')")
+			dataType, opts, err := ParseColumnDataType("SET('A','B','C')", nil)
 			assert.NoError(t, err)
 			assert.Equal(t, Set, dataType)
 			assert.Equal(t, &Opts{EnumValues: []string{"A", "B", "C"}}, opts)
@@ -163,7 +163,7 @@ func TestParseColumnDataType(t *testing.T) {
 	{
 		// Blob
 		for _, blob := range []string{"blob", "tinyblob", "mediumblob", "longblob"} {
-			dataType, _, err := ParseColumnDataType(blob)
+			dataType, _, err := ParseColumnDataType(blob, nil)
 			assert.NoError(t, err)
 			assert.Equal(t, Blob, dataType, blob)
 		}
