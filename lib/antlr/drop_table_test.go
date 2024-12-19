aabbe48164cb2a23c33e8d5369b7d633fd02198e
Robin Tang <rtang.cs@gmail.com>
2024-12-19 13:11:51 -0800

    Remove.

--- lib/antlr/drop_table_test.go
+++ lib/antlr/drop_table_test.go
@@ -8,16 +8,6 @@ import (
 
 func TestDropTable(t *testing.T) {
 	{
-		// Drop table specify schema
-		events, err := Parse("DROP TABLE db_name.table_name;")
-		assert.NoError(t, err)
-		assert.Len(t, events, 1)
-
-		dropTableEvent, isOk := events[0].(DropTableEvent)
-		assert.True(t, isOk)
-		assert.Equal(t, "table_name", dropTableEvent.GetTable())
-	}
-	{
 		// Single drop table
 		for _, tblName := range []string{"table_name", "`table_name`", "db_name.table_name", "`db_name`.`table_name`"} {
 			events, err := Parse(fmt.Sprintf("DROP TABLE %s;", tblName))
