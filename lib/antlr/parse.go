440e093ed507e744b145c3ae876619f8af55bff2
Robin Tang <rtang.cs@gmail.com>
2024-12-20 11:29:28 -0800

    [MySQL] Ignore more context types that we don't care about. (#626)

--- lib/antlr/parse.go
+++ lib/antlr/parse.go
@@ -86,6 +86,8 @@ func visit(tree antlr.Tree) ([]Event, error) {
 	case *generated.RenameTableContext:
 		return processRenameTable(ctx)
 	case
+		*generated.CreateIndexContext,
+		*generated.DropIndexContext,
 		*generated.CreateEventContext,
 		*generated.DropEventContext,
 		*generated.EmptyStatement_Context,
